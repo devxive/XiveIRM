45bfdc5094523242da39df0f94bce070995cded6
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-05-29 00:09:28 +0200

    added audit form to admin

---
+++ source/package/components/com_xiveirm/administrator/controllers/audit.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @version     3.0.4
+ * @package     com_xiveirm
+ * @copyright   Copyright (C) 1997 - 2013 by devXive - research and development. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @author      devXive <support@devxive.com> - http://devxive.com
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.controllerform');
+
+/**
+ * Audit controller class.
+ */
+class XiveirmControllerAudit extends JControllerForm
+{
+
+    function __construct() {
+        $this->view_list = 'audits';
+        parent::__construct();
+    }
+
+}
