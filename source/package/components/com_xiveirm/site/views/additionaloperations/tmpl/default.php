3fcd1d7e8b315cb2c79f9b77450a943bc017d405
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-05-29 00:38:53 +0200

    added "additionaloperation" view only, for testing

---
+++ source/package/components/com_xiveirm/site/views/additionaloperations/tmpl/default.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @version     3.0.4
+ * @package     com_xiveirm
+ * @copyright   Copyright (C) 1997 - 2013 by devXive - research and development. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @author      devXive <support@devxive.com> - http://devxive.com
+ */
+// no direct access
+defined('_JEXEC') or die;
+?>
+
+
+
