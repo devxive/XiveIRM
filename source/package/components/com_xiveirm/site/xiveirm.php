52d913bb622b4d28f3ec738b29795d93026110d4
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-05-29 00:44:53 +0200

    added helper classes

--- source/package/components/com_xiveirm/site/xiveirm.php
+++ source/package/components/com_xiveirm/site/xiveirm.php
@@ -9,6 +9,10 @@
 
 defined('_JEXEC') or die;
 
+require_once JPATH_COMPONENT.'/helpers/xiveirm.php';
+require_once JPATH_COMPONENT.'/helpers/irmsystem.php';
+$irm = new IRMSystem();
+
 // Include dependancies
 jimport('joomla.application.component.controller');
 
