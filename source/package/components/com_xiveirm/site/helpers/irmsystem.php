f472e5e60eaa62e41b92394b7c447d4478fe8b31
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-06-02 23:40:55 +0200

    added getTabData method

--- source/package/components/com_xiveirm/site/helpers/irmsystem.php
+++ source/package/components/com_xiveirm/site/helpers/irmsystem.php
@@ -11,9 +11,54 @@ defined('_JEXEC') or die;
 
 class IRMSystem
 {
-	public function getUserName($id) {
+	public function getUserName($id)
+	{
 		return JFactory::getUser($id)->name;
 	}
 
+	/*
+	 * 
+	 * returns a prepared array
+	 */
+	public function getTabData($tabAppId, $masterDataItemId, $json = false)
+	{
+		if(!$tabAppId || !$masterDataItemId)
+		{
+			return false;
+		}
+
+		// set the tab_id for the where clause
+		$tabId = $tabAppId . '.' . $masterDataItemId;
+
+		$db = JFactory::getDBO();
+		$query = $db->getQuery(true);
+
+		$query
+			->select('*')
+			->from('#__xiveirm_masterdata_add')
+			->where('tab_id = ' . $db->quote($tabId) . '');
+		$db->setQuery($query);
+
+		// Try to get the data or the error code for debugging
+		try
+		{
+			$result = $db->loadObject();
+
+			if($json == false && $result)
+			{
+				$tab_value = json_decode($result->tab_value);
+				$result->tab_value = $tab_value;
+			}			
+
+			return $result;
+		} catch (Exception $e) {
+			$error = array();
+			$error['code'] = (int)$e->getCode();
+			$error['message'] = $e->getMessage();
+
+			return $error;
+		}
+		
+	}
 }
 
