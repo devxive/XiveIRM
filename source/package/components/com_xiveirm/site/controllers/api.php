95cd1e981d6a633e8e476275867651a42b44a251
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-06-02 23:41:35 +0200

    updated to work with ajax requests

--- source/package/components/com_xiveirm/site/controllers/api.php
+++ source/package/components/com_xiveirm/site/controllers/api.php
@@ -25,30 +25,6 @@ class XiveirmControllerApi extends XiveirmController
 	 */
 	public function edit()
 	{
-		$app			= JFactory::getApplication();
-
-		// Get the previous edit id (if any) and the current edit id.
-		$previousId = (int) $app->getUserState('com_xiveirm.edit.api.id');
-		$editId	= JFactory::getApplication()->input->getInt('id', null, 'array');
-
-		// Set the user id for the user to edit in the session.
-		$app->setUserState('com_xiveirm.edit.api.id', $editId);
-
-		// Get the model.
-		$model = $this->getModel('Api', 'XiveirmModel');
-
-		// Check out the item
-		if ($editId) {
-            $model->checkout($editId);
-		}
-
-		// Check in the previous user.
-		if ($previousId) {
-            $model->checkin($previousId);
-		}
-
-		// Redirect to the edit screen.
-		$this->setRedirect(JRoute::_('index.php?option=com_xiveirm&view=apiform&layout=edit', false));
 	}
 
 	/**
@@ -59,219 +35,45 @@ class XiveirmControllerApi extends XiveirmController
 	 */
 	public function save()
 	{
-// 
-// 		// Check for request forgeries.
-// 		JSession::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
-// 
-// 		// Initialise variables.
-// 		$app	= JFactory::getApplication();
-// 		$model = $this->getModel('Api', 'XiveirmModel');
-// 
-// 		// Get the user data.
-//		$data = JFactory::getApplication()->input->get('tabForm', array(), 'array');
+		$data = array('direction' => 'ajax');
 
-		$data = array('itemid' => '1', 'tabid' => 'medicaltesttab', 'direction' => 'api', 'format' => 'json', 'first_name' => 'Max', 'last_name' => 'Mustermann');
-		// Store the identifier vars
-		$itemId = $data['itemid'];
-		$tabId = $data['tabid'];
-		$direction = $data['direction'];
-		$format = $data['format'];
-		
-		// Build a new array if stored in a single row as json encoded values
-		if($format == 'json') {
-			$newArray = array();
-			foreach($data as $key => $val)
-			{
-				if($key != 'itemid' || $key != 'tabid' || $key != 'direction' || $key != 'format' || $key != '')
-				{
-					$newArray[$key] = $val;
-				}
-			}
-		}
-
-		$dbValue = json_encode($newArray);
-
-		$db = JFactory::getDBO();
-		$query = $db->getQuery(true);
-		
-		// check if there are already datas from the specific tabid related to customers itemId
-		$query
-			->select('*')
-			->from('#__xiveirm_masterdata_add')
-			->where('item_id = ' . $itemId)
-			->where('tab_name = ' . $tabId);
-		$db->setQuery($query);
-
-		if($db->loadObjectList()) { echo 'NIX DAAAAAAAAAAAAA'; }
-
-		// Validate the posted data and check what method we have to use to store values in the database
-		if($direction == 'api') {
-			$return_arr = array();
-			$return_arr["code"] = '200';
-			$return_arr["message"] = 'Everything ok';
-			echo json_encode($return_arr);
-		} else if($direction == 'app') {
-			echo '<p class="alert alert-notice">Single Values</p>';
-		} else {
-//			JError::raiseError(500, 'We cant provide a query!');
-//			return false;
-		}
-
-
-
-// 
-// 		// Validate the posted data.
-// 		$form = $model->getForm();
-// 		if (!$form) {
-// 			JError::raiseError(500, $model->getError());
-// 			return false;
-// 		}
-// 
-// 		// Validate the posted data.
-// 		$data = $model->validate($form, $data);
-// 
-// 		// Check for errors.
-// 		if ($data === false) {
-// 			// Get the validation messages.
-// 			$errors	= $model->getErrors();
-// 
-// 			// Push up to three validation messages out to the user.
-// 			for ($i = 0, $n = count($errors); $i < $n && $i < 3; $i++) {
-// 				if ($errors[$i] instanceof Exception) {
-// 					$app->enqueueMessage($errors[$i]->getMessage(), 'warning');
-// 				} else {
-// 					$app->enqueueMessage($errors[$i], 'warning');
-// 				}
-// 			}
-// 
-// 			// Save the data in the session.
-// 			$app->setUserState('com_xiveirm.edit.api.data', JRequest::getVar('jform'),array());
-// 
-// 			// Redirect back to the edit screen.
-// 			$id = (int) $app->getUserState('com_xiveirm.edit.api.id');
-// 			$this->setRedirect(JRoute::_('index.php?option=com_xiveirm&view=api&layout=edit&id='.$id, false));
-// 			return false;
-// 		}
-// 
-// 		// Attempt to save the data.
-// 		$return	= $model->save($data);
-// 
-// 		// Check for errors.
-// 		if ($return === false) {
-// 			// Save the data in the session.
-// 			$app->setUserState('com_xiveirm.edit.api.data', $data);
-// 
-// 			// Redirect back to the edit screen.
-// 			$id = (int)$app->getUserState('com_xiveirm.edit.api.id');
-// 			$this->setMessage(JText::sprintf('Save failed', $model->getError()), 'warning');
-// 			$this->setRedirect(JRoute::_('index.php?option=com_xiveirm&view=api&layout=edit&id='.$id, false));
-// 			return false;
-// 		}
-// 
-//             
-//         // Check in the profile.
-//         if ($return) {
-//             $model->checkin($return);
-//         }
-//         
-//         // Clear the profile id from the session.
-//         $app->setUserState('com_xiveirm.edit.api.id', null);
-// 
-//         // Redirect to the list screen.
-//         $this->setMessage(JText::_('COM_XIVEIRM_ITEM_SAVED_SUCCESSFULLY'));
-//         $menu = & JSite::getMenu();
-//         $item = $menu->getActive();
-//         $this->setRedirect(JRoute::_($item->link, false));
-// 
-// 		// Flush the data from the session.
-// 		$app->setUserState('com_xiveirm.edit.api.data', null);
-	}
-    
-    
-    function cancel() {
-		$menu = & JSite::getMenu();
-        $item = $menu->getActive();
-        $this->setRedirect(JRoute::_($item->link, false));
-    }
-    
-	public function remove()
-	{
 		// Check for request forgeries.
-		JSession::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
+		if($data['direction'] == 'ajax')
+		{
+			// This is the api direction (where the data come from "ajax" or "in future whatever" we use at this time only the ajax variant)
+			// The problem with JSession token check have to be resolved!
+		}
+		else
+		{
+			JSession::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
+		}
 
 		// Initialise variables.
-		$app	= JFactory::getApplication();
+		$app = JFactory::getApplication();
 		$model = $this->getModel('Api', 'XiveirmModel');
 
-		// Get the user data.
-		$data = JFactory::getApplication()->input->get('jform', array(), 'array');
-
-		// Validate the posted data.
-		$form = $model->getForm();
-		if (!$form) {
-			JError::raiseError(500, $model->getError());
-			return false;
-		}
-
-		// Validate the posted data.
-		$data = $model->validate($form, $data);
-
-		// Check for errors.
-		if ($data === false) {
-			// Get the validation messages.
-			$errors	= $model->getErrors();
-
-			// Push up to three validation messages out to the user.
-			for ($i = 0, $n = count($errors); $i < $n && $i < 3; $i++) {
-				if ($errors[$i] instanceof Exception) {
-					$app->enqueueMessage($errors[$i]->getMessage(), 'warning');
-				} else {
-					$app->enqueueMessage($errors[$i], 'warning');
-				}
-			}
-
-			// Save the data in the session.
-			$app->setUserState('com_xiveirm.edit.api.data', $data);
-
-			// Redirect back to the edit screen.
-			$id = (int) $app->getUserState('com_xiveirm.edit.api.id');
-			$this->setRedirect(JRoute::_('index.php?option=com_xiveirm&view=api&layout=edit&id='.$id, false));
-			return false;
-		}
+ 		// Get the tabForm data.
+		$data = JFactory::getApplication()->input->get('tabForm', array(), 'array');
 
 		// Attempt to save the data.
-		$return	= $model->delete($data);
+		$return = $model->save($data);
 
-		// Check for errors.
-		if ($return === false) {
-			// Save the data in the session.
-			$app->setUserState('com_xiveirm.edit.api.data', $data);
+//		echo json_encode($return);
 
-			// Redirect back to the edit screen.
-			$id = (int)$app->getUserState('com_xiveirm.edit.api.id');
-			$this->setMessage(JText::sprintf('Delete failed', $model->getError()), 'warning');
-			$this->setRedirect(JRoute::_('index.php?option=com_xiveirm&view=api&layout=edit&id='.$id, false));
-			return false;
-		}
+//		// XAP-TODO: Not so nice and have to be updated to the right things
+		$returnUrl = '/components/com_xiveirm/helpers/api_return.php?string=' . json_encode($return);
+		header('Location: ' . $returnUrl);
 
-            
-        // Check in the profile.
-        if ($return) {
-            $model->checkin($return);
-        }
-        
-        // Clear the profile id from the session.
-        $app->setUserState('com_xiveirm.edit.api.id', null);
-
-        // Redirect to the list screen.
-        $this->setMessage(JText::_('COM_XIVEIRM_ITEM_DELETED_SUCCESSFULLY'));
-        $menu = & JSite::getMenu();
-        $item = $menu->getActive();
-        $this->setRedirect(JRoute::_($item->link, false));
-
-		// Flush the data from the session.
-		$app->setUserState('com_xiveirm.edit.api.data', null);
+//		// Flush the data from the session.
+//		$app->setUserState('com_xiveirm.edit.api.data', null);
 	}
     
     
+	function cancel()
+	{
+	}
+
+	public function remove()
+	{
+	}
 }
--- source/package/components/com_xiveirm/site/models/api.php
+++ source/package/components/com_xiveirm/site/models/api.php
@@ -18,9 +18,8 @@ jimport('joomla.event.dispatcher');
  */
 class XiveirmModelApi extends JModelForm
 {
-    
-    var $_item = null;
-    
+	var $_item = null;
+
 	/**
 	 * Method to auto-populate the model state.
 	 *
@@ -30,27 +29,8 @@ class XiveirmModelApi extends JModelForm
 	 */
 	protected function populateState()
 	{
-		$app = JFactory::getApplication('com_xiveirm');
-
-		// Load state from the request userState on edit or from the passed variable on default
-        if (JFactory::getApplication()->input->get('layout') == 'edit') {
-            $id = JFactory::getApplication()->getUserState('com_xiveirm.edit.api.id');
-        } else {
-            $id = JFactory::getApplication()->input->get('id');
-            JFactory::getApplication()->setUserState('com_xiveirm.edit.api.id', $id);
-        }
-		$this->setState('api.id', $id);
-
-		// Load the parameters.
-		$params = $app->getParams();
-        $params_array = $params->toArray();
-        if(isset($params_array['item_id'])){
-            $this->setState('api.id', $params_array['item_id']);
-        }
-		$this->setState('params', $params);
-
+		echo '<h1>YOU ARE HERE => model->populateState</h1>';
 	}
-        
 
 	/**
 	 * Method to get an ojbect.
@@ -61,46 +41,14 @@ class XiveirmModelApi extends JModelForm
 	 */
 	public function &getData($id = null)
 	{
-		if ($this->_item === null)
-		{
-			$this->_item = false;
-
-			if (empty($id)) {
-				$id = $this->getState('api.id');
-			}
-
-			// Get a level row instance.
-			$table = $this->getTable();
-
-			// Attempt to load the row.
-			if ($table->load($id))
-			{
-				// Check published state.
-				if ($published = $this->getState('filter.published'))
-				{
-					if ($table->state != $published) {
-						return $this->_item;
-					}
-				}
-
-				// Convert the JTable to a clean JObject.
-				$properties = $table->getProperties(1);
-				$this->_item = JArrayHelper::toObject($properties, 'JObject');
-			} elseif ($error = $table->getError()) {
-				$this->setError($error);
-			}
-		}
-
-		return $this->_item;
+		echo '<h1>YOU ARE HERE => model->getData</h1>';
 	}
-    
-	public function getTable($type = 'Api', $prefix = 'XiveirmTable', $config = array())
-	{   
-        $this->addTablePath(JPATH_COMPONENT_ADMINISTRATOR.'/tables');
-        return JTable::getInstance($type, $prefix, $config);
-	}     
 
-    
+	public function getTable($type = 'Api', $prefix = 'XiveirmTable', $config = array())
+	{
+		echo '<h1>YOU ARE HERE => model->getTable</h1>';
+	}
+
 	/**
 	 * Method to check in an item.
 	 *
@@ -110,24 +58,7 @@ class XiveirmModelApi extends JModelForm
 	 */
 	public function checkin($id = null)
 	{
-		// Get the id.
-		$id = (!empty($id)) ? $id : (int)$this->getState('api.id');
-
-		if ($id) {
-            
-			// Initialise the table
-			$table = $this->getTable();
-
-			// Attempt to check the row in.
-            if (method_exists($table, 'checkin')) {
-                if (!$table->checkin($id)) {
-                    $this->setError($table->getError());
-                    return false;
-                }
-            }
-		}
-
-		return true;
+		echo '<h1>YOU ARE HERE => model->checkin</h1>';
 	}
 
 	/**
@@ -139,34 +70,14 @@ class XiveirmModelApi extends JModelForm
 	 */
 	public function checkout($id = null)
 	{
-		// Get the user id.
-		$id = (!empty($id)) ? $id : (int)$this->getState('api.id');
-
-		if ($id) {
-            
-			// Initialise the table
-			$table = $this->getTable();
-
-			// Get the current user object.
-			$user = JFactory::getUser();
-
-			// Attempt to check the row out.
-            if (method_exists($table, 'checkout')) {
-                if (!$table->checkout($user->get('id'), $id)) {
-                    $this->setError($table->getError());
-                    return false;
-                }
-            }
-		}
-
-		return true;
+		echo '<h1>YOU ARE HERE => model->checkout</h1>';
 	}    
-    
+
 	/**
 	 * Method to get the profile form.
 	 *
 	 * The base form is loaded from XML 
-     * 
+	 * 
 	 * @param	array	$data		An optional array of data for the form to interogate.
 	 * @param	boolean	$loadData	True if the form is to load its own data (default case), false if not.
 	 * @return	JForm	A JForm object on success, false on failure
@@ -174,13 +85,7 @@ class XiveirmModelApi extends JModelForm
 	 */
 	public function getForm($data = array(), $loadData = true)
 	{
-		// Get the form.
-		$form = $this->loadForm('com_xiveirm.api', 'api', array('control' => 'jform', 'load_data' => $loadData));
-		if (empty($form)) {
-			return false;
-		}
-
-		return $form;
+		echo '<h1>YOU ARE HERE => model->getForm</h1>';
 	}
 
 	/**
@@ -191,9 +96,7 @@ class XiveirmModelApi extends JModelForm
 	 */
 	protected function loadFormData()
 	{
-		$data = $this->getData(); 
-        
-        return $data;
+		echo '<h1>YOU ARE HERE => model->loadFormData</h1>';
 	}
 
 	/**
@@ -205,64 +108,164 @@ class XiveirmModelApi extends JModelForm
 	 */
 	public function save($data)
 	{
-		$id = (!empty($data['id'])) ? $data['id'] : (int)$this->getState('api.id');
-        $state = (!empty($data['state'])) ? 1 : 0;
-        $user = JFactory::getUser();
+		// Now we get raw $data from the controller and have to perform the save request
+		// first we have to split the data into separate values
 
-        if($id) {
-            //Check the user can edit this item
-            $authorised = $user->authorise('core.edit', 'com_xiveirm') || $authorised = $user->authorise('core.edit.own', 'com_xiveirm');
-            if($user->authorise('core.edit.state', 'com_xiveirm') !== true && $state == 1){ //The user cannot edit the state of the item.
-                $data['state'] = 0;
-            }
-        } else {
-            //Check the user can create new items in this section
-            $authorised = $user->authorise('core.create', 'com_xiveirm');
-            if($user->authorise('core.edit.state', 'com_xiveirm') !== true && $state == 1){ //The user cannot edit the state of the item.
-                $data['state'] = 0;
-            }
-        }
+		//check if we get any data
+		if(!$data)
+		{
+			// Perform the return array
+			$return_arr = array();
+			$return_arr["apiReturnCode"] = 505;
+			$return_arr["apiReturnRowId"] = null;
+			$return_arr["apiReturnMessage"] = 'There is eiter an api nor an app request given!';
 
-        if ($authorised !== true) {
-            JError::raiseError(403, JText::_('JERROR_ALERTNOAUTHOR'));
-            return false;
-        }
-        
-        $table = $this->getTable();
-        if ($table->save($data) === true) {
-            return $id;
-        } else {
-            return false;
-        }
-        
+			return $return_arr;
+		}
+
+		// Store the identifier vars
+		$dataTabId		= $data['id']; // This is the data tab id, the id from the masterdata_add db row
+		$tabAppId		= $data['tabappid']; // This is the tab identifier (a lowercase name => the same as in plugins/masterdatatabs)
+		$masterDataItemId	= $data['masterdataitemid']; // This is the item dbId from the #__xiveirm_masterdata table
+		$direction		= $data['direction']; // This is the api direction (where the data come from "ajax" or "in future whatever" we use at this time only the ajax variant)
+		$valueFormat		= $data['valueformat']; // The format we use to store the submitted values in the database
+
+		// set the tab_id for the database
+		$tabId = $tabAppId . '.' . $masterDataItemId;
+
+		// exclude system based infos for the new array
+		unset($data['id']);
+		unset($data['tabappid']);
+		unset($data['masterdataitemid']);
+		unset($data['direction']);
+		unset($data['valueformat']);
+
+		// Check if in all the datas are values and/or nested arrays from multiselect and reinject the val as key
+		$newDataArray = array();
+		foreach($data as $key => $val)
+		{
+			if(!empty($val))
+			{
+				if(is_array($val))
+				{
+					$newKey = $key . '_set';
+					foreach($val as $newVal)
+					{
+						$newDataArray[$newKey][$newVal] = true;
+					}
+				}
+				else
+				{
+					$newDataArray[$key] = $val;
+				}
+			}
+		}
+
+		// Build a new array if stored in a single row or as json encoded values || At this time json is the only way to store values in the database
+		if($valueFormat == 'json')
+		{
+			$newData = json_encode($newDataArray);
+		}
+
+		// Lets save the data in the database
+		$db = JFactory::getDBO();
+		$query = $db->getQuery(true);
+
+		// Lets have a look if we have already a tab saved for this customer! Happens if the user attemp to click more than once on save
+		if($dataTabId == '0')
+		{
+			$query
+				->select('*')
+				->from('#__xiveirm_masterdata_add')
+				->where('tab_id = ' . $db->quote($tabId) . '');
+			$db->setQuery($query);
+
+			$dataTabId = $db->loadObject()->id;
+		}
+
+		if($dataTabId == '0' || !$dataTabId)
+		{
+			// Set the columns
+			$columns = array('tab_id', 'tab_field_id', 'tab_value', 'state', 'ordering');
+
+			// Set the values
+			$values = array($db->quote($tabId), 0, $db->quote($newData), 1, 0);
+
+			$query
+				->insert($db->quoteName('#__xiveirm_masterdata_add'))
+				->columns($db->quoteName($columns))
+				->values(implode(',', $values));
+			$db->setQuery($query);
+
+			// Try to store or get the error code for debugging
+			try
+			{
+				$db->execute();
+				$apiReturnCode = 'SAVED';
+				$apiReturnRowId = $db->insertid();
+				$apiReturnMessage = 'Succesfully saved';
+			} catch (Exception $e) {
+				$apiReturnRowId = null;
+				$apiReturnCode = (int)$e->getCode();
+				$apiReturnMessage = $e->getMessage();
+			}
+		}
+		else if($dataTabId > '0')
+		{
+			// Set the fields
+			$fields = array(
+				'tab_id = ' . $db->quote($tabId) . '',
+				'tab_field_id = 0',
+				'tab_value = ' . $db->quote($newData) . '',
+				'state = 1',
+				'ordering = 0');
+
+			$query
+				->update($db->quoteName('#__xiveirm_masterdata_add'))
+				->set($fields)
+				->where('id = ' . $db->quote($dataTabId) . '');
+
+			$db->setQuery($query);
+
+			// Try to store or get the error code for debugging
+			try
+			{
+				$db->execute();
+				$apiReturnCode = 'UPDATED';
+				$apiReturnRowId = $dataTabId;
+				$apiReturnMessage = 'Succesfully updated';
+			} catch (Exception $e) {
+				$apiReturnCode = (int)$e->getCode();
+				$apiReturnRowId = null;
+				$apiReturnMessage = $e->getMessage();
+			}
+		}
+		else
+		{
+			$apiReturnRowId = null;
+			$apiReturnCode = 666;
+			$apiReturnMessage = 'The dataTabId is either 0 nor an integer greater than 0';
+
+//			JError::raiseError(500, 'There is either an api nor an app request given!');
+//			return false;
+		}
+
+		// Perform the return array
+		$return_arr = array();
+		$return_arr["apiReturnCode"] = $apiReturnCode;
+		$return_arr["apiReturnRowId"] = $apiReturnRowId;
+		$return_arr["apiReturnMessage"] = $apiReturnMessage;
+
+		return $return_arr;
 	}
-    
-     function delete($data)
-    {
-        $id = (!empty($data['id'])) ? $data['id'] : (int)$this->getState('api.id');
-        if(JFactory::getUser()->authorise('core.delete', 'com_xiveirm') !== true){
-            JError::raiseError(403, JText::_('JERROR_ALERTNOAUTHOR'));
-            return false;
-        }
-        $table = $this->getTable();
-        if ($table->delete($data['id']) === true) {
-            return $id;
-        } else {
-            return false;
-        }
-        
-        return true;
-    }
-    
-    function getCategoryName($id){
-        $db = JFactory::getDbo();
-        $query = $db->getQuery(true);
-        $query 
-            ->select('title')
-            ->from('#__categories')
-            ->where('id = ' . $id);
-        $db->setQuery($query);
-        return $db->loadObject();
-    }
-    
+
+	function delete($data)
+	{
+		echo '<h1>YOU ARE HERE => model->delete</h1>';
+	}
+
+	function getCategoryName($id)
+	{
+		echo '<h1>YOU ARE HERE => model->getCategoryName</h1>';
+	}
 }
