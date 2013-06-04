420162051eabf81658a342b0309afd5939976e8b
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-06-04 11:55:27 +0200

    added testcase

--- source/package/components/com_xiveirm/site/helpers/api_return.php
+++ source/package/components/com_xiveirm/site/helpers/api_return.php
@@ -1,5 +1,8 @@
 <?php
 header('Content-type: text/json');
 
+// For testing purposes!
+// echo '{"apiReturnCode":"0815-4711","apiReturnRowId":"0","apiReturnMessage":"Du bist bl&ouml;d ;)"}';
+
 echo $_GET[string];
 ?>
