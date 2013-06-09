7217b544bd92667f831fe6114f1a95f642ece74f
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-06-09 16:31:48 +0200

    added access_id, reinit and prepared for 3.1

--- source/package/components/com_medtranscoord/administrator/tables/order.php
+++ source/package/components/com_medtranscoord/administrator/tables/order.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @version     3.0.0
+ * @version     3.1.0
  * @package     com_medtranscoord
  * @copyright   Copyright (c) 1997 - 2013 by devXive - research and development. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
