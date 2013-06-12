e46e31c9cae276871161d9c3ac25e3f3b3f3de62
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-06-12 22:23:40 +0200

    changed error message if a user already checked out the customer

--- source/package/components/com_xiveirm/site/views/irmcustomerform/tmpl/default.php
+++ source/package/components/com_xiveirm/site/views/irmcustomerform/tmpl/default.php
@@ -20,6 +20,13 @@ $lang->load('com_xiveirm', JPATH_ADMINISTRATOR);
 JPluginHelper::importPlugin( 'irmcustomertabs' ); // returned 1 if get successfully loaded
 JPluginHelper::importPlugin( 'irmcustomerwidgets' ); // returned 1 if get successfully loaded
 $dispatcher = JDispatcher::getInstance();
+
+// used for javascript processed messages
+$full_name = $this->item->first_name . ' ' . $this->item->last_name;
+
+// check if the customer is checked out and compare user id. If customer is checked out by the same user, show no info! TODO: Future versions may show a message that the user have to save or click cancel!!!!
+if($this->item->checked_out != 0 && IRMSystem::getUserName($this->item->checked_out) != IRMSystem::getUserName(null)) { $checked_out = true; } else { $checked_out = false; }
+
 ?>
 <!--
 <script type="text/javascript">
@@ -65,32 +72,35 @@ $dispatcher = JDispatcher::getInstance();
 				<?php endif; ?>
 			</span>
 			<span class="span5">
-				<a class="btn btn-danger btn-mini pull-right inline" href="<?php echo JRoute::_('index.php?option=com_xiveirm&task=irmcustomer.cancel'); ?>" data-rel="tooltip" data-original-title="<?php echo JText::_('COM_XIVEIRM_IRMCUSTOMER_FORM_CANCEL_CHECKIN_TIP'); ?>"><i class="icon-reply"></i> <?php echo JText::_('COM_XIVEIRM_IRMCUSTOMER_FORM_CANCEL_CHECKIN'); ?></a>
+				<div class="btn-group pull-right inline">
+					<?php if($this->item->id && !$checked_out): ?>
+						<a onClick="enableForm()" id="loading-btn-edit" data-loading-text="<?php echo JText::_('COM_XIVEIRM_API_PLEASE_WAIT_BUTTON'); ?>" data-error-text="<?php echo JText::_('COM_XIVEIRM_API_ERROR_TRY_AGAIN_BUTTON'); ?>" class="btn btn-warning btn-mini edit-form-button"><i class="icon-edit"></i> <?php echo JText::_('COM_XIVEIRM_EDIT_ITEM'); ?></a>
+					<?php endif; ?>
+					<a class="btn btn-danger btn-mini" href="<?php echo JRoute::_('index.php?option=com_xiveirm&task=irmcustomer.cancel'); ?>"><i class="icon-reply"></i> <?php echo JText::_('COM_XIVEIRM_CANCEL_ITEM'); ?></a>
+				</div>
 			</span>
 		</h1>
 	</div><!--/header-->
 	<!-- ---------- ---------- ---------- ---------- ---------- END PAGE HEADER ---------- ---------- ---------- ---------- ---------- -->
 	<!-- ---------- ---------- ---------- ---------- ---------- BEGIN CHECK_OUT MESSAGE ---------- ---------- ---------- ---------- ---------- -->
-	<?php if($this->item->checked_out): ?>
-		<?php if(IRMSystem::getUserName($this->item->checked_out) == IRMSystem::getUserName(null)): ?>
-			<div id="checkout-info" class="alert alert-info" style="display: none;">
-				<button type="button" class="close" data-dismiss="alert">
-					<i class="icon-remove"></i>
-				</button>
-				<?php echo JText::_('COM_XIVEIRM_IRMCUSTOMER_FORM_CHECKED_OUT_ALERT_INFO'); ?>
-			</div>
-		<?php else : ?>
-			<div id="checkout-info" class="alert alert-error" style="display: none;">
-				<button type="button" class="close" data-dismiss="alert">
-					<i class="icon-remove"></i>
-				</button>
-				<?php echo JText::printf('COM_XIVEIRM_IRMCUSTOMER_FORM_CHECKED_OUT_ALERT_ERROR', '#', IRMSystem::getUserName($this->item->checked_out), $this->item->checked_out_time, '5'); // 1. Link zum Benutzer, 2. Name, 3. Checkedout time, 4. wann wieder verfügbar ?>
-			</div>
-		<?php endif; ?>
+	<?php if($checked_out): ?>
+		<div id="checkout-info" class="alert alert-error" style="display: none;">
+			<button type="button" class="close" data-dismiss="alert">
+				<i class="icon-remove"></i>
+			</button>
+			<?php
+				$user_profile_url = '/#';
+				$users_name = IRMSystem::getUserName($this->item->checked_out);
+				$checked_out_time = $this->item->checked_out_time;
+				$checkin_in = '5';
+				echo '<h1><i class=\"icon-signout\"></i> ' . JText::_('COM_XIVEIRM_IRMCUSTOMER_FORM_CHECKED_OUT_ALERT_ERROR_TITLE') . '</h1>';
+				echo '<p>' . JText::sprintf('COM_XIVEIRM_IRMCUSTOMER_FORM_CHECKED_OUT_ALERT_ERROR_BODY', $user_profile_url, $users_name, $checked_out_time, $checkin_in) . '</p>';
+			?>
+		</div>
 	<?php endif; ?>
 	<!-- ---------- ---------- ---------- ---------- ---------- END CHECK_OUT MESSAGE ---------- ---------- ---------- ---------- ---------- -->
 
-	<form id="form-irmcustomer" action="<?php echo JRoute::_('index.php?option=com_xiveirm&task=irmcustomer.save'); ?>" method="post" class="form-validate form-horizontal" enctype="multipart/form-data">
+	<form id="form-irmcustomer" class="form-validate form-horizontal">
 
 		<!-- ---------- ---------- ---------- ---------- ---------- BEGIN MASTER_TAP_PANE_PLUGINSTYLED ---------- ---------- ---------- ---------- ---------- -->
 		<div class="tabbable">
@@ -122,15 +132,6 @@ $dispatcher = JDispatcher::getInstance();
 				<div id="base-data" class="tab-pane active">
 					<div class="row-fluid">
 						<div class="span7">
-							<?php if($this->item->id): ?>
-								<div class="control-group">
-									<label id="input-control-button" class="control-label red"><b>Edit form</b></label>
-									<div class="controls">
-										<input id="id-disable-check" class="ace-switch ace-switch-7" type="checkbox"><span class="lbl"></span>
-									</div>
-								</div>
-							<?php endif; ?>
-							
 							<div class="control-group">
 								<label class="control-label"><?php echo JText::_('COM_XIVEIRM_IRMCUSTOMER_FORM_CUSTOMER_ID_LABEL'); ?>, <?php echo JText::_('COM_XIVEIRM_IRMCUSTOMER_FORM_NAME_LABEL'); ?></label>
 								<div class="controls controls-row">
@@ -144,7 +145,7 @@ $dispatcher = JDispatcher::getInstance();
 									<input type="text" name="coreform[title]" class="input-control span6" placeholder="<?php echo JText::_('COM_XIVEIRM_IRMCUSTOMER_FORM_NAME_TITLE'); ?>" value="<?php echo $this->item->title; ?>">
 								</div>
 								<div class="controls controls-row">
-									<input type="text" name="coreform[last_name]" class="input-control span6" placeholder="<?php echo JText::_('COM_XIVEIRM_IRMCUSTOMER_FORM_LAST_NAME'); ?>" value="<?php echo $this->item->last_name; ?>" autofocus>
+									<input type="text" name="coreform[last_name]" class="input-control span6" placeholder="<?php echo JText::_('COM_XIVEIRM_IRMCUSTOMER_FORM_LAST_NAME'); ?>" value="<?php echo $this->item->last_name; ?>" <?php echo empty($this->item->id) ? 'autofocus' : ''; ?>>
 									<input type="text" name="coreform[first_name]" class="input-control span6" placeholder="<?php echo JText::_('COM_XIVEIRM_IRMCUSTOMER_FORM_FIRST_NAME'); ?>" value="<?php echo $this->item->first_name; ?>">
 								</div>
 							</div>
@@ -300,12 +301,12 @@ $dispatcher = JDispatcher::getInstance();
 
 				<div class="form-actions">
 					<span id="form-buttons" class="<?php echo empty($this->item->id) ? '' : 'hidden'; ?>">
-						<button id="loading-btn-recall" data-loading-text="Please wait..." data-complete-text="Saved" data-error-text="Error!" class="validate btn btn-info" type="submit" data-rel="tooltip" data-original-title="<?php echo JText::_('COM_XIVEIRM_IRMCUSTOMER_FORM_SUBMIT_CHECKIN_TIP'); ?>"><i class="icon-ok"></i> <?php echo isset($this->item->id) ? 'Update' : JText::_('COM_XIVEIRM_IRMCUSTOMER_FORM_SUBMIT_CHECKIN'); ?></button>
+						<button id="loading-btn-save" data-loading-text="<?php echo JText::_('COM_XIVEIRM_API_PLEASE_WAIT_BUTTON'); ?>" data-complete-text="<?php echo JText::_('COM_XIVEIRM_API_SAVED_BUTTON'); ?>" data-error-text="<?php echo JText::_('COM_XIVEIRM_API_ERROR_TRY_AGAIN_BUTTON'); ?>" class="validate btn btn-info" type="submit"><i class="icon-ok"></i> <?php echo isset($this->item->id) ? JText::_('COM_XIVEIRM_UPDATE_ITEM') : JText::_('COM_XIVEIRM_SAVE_ITEM'); ?></button>
 						&nbsp; &nbsp; &nbsp;
 						<button class="btn" type="reset" data-rel="tooltip" data-original-title="<?php echo JText::_('COM_XIVEIRM_IRMCUSTOMER_FORM_RESET_TIP'); ?>"><i class="icon-undo"></i> <?php echo JText::_('COM_XIVEIRM_IRMCUSTOMER_FORM_RESET'); ?></button>
 						&nbsp; &nbsp; &nbsp;
 					</span>
-					<a href="<?php echo JRoute::_('index.php?option=com_xiveirm&task=irmcustomer.cancel'); ?>" data-rel="tooltip" data-original-title="<?php echo JText::_('COM_XIVEIRM_IRMCUSTOMER_FORM_CANCEL_CHECKIN_TIP'); ?>" class="btn btn-danger"><i class="icon-reply"></i> <?php echo JText::_('COM_XIVEIRM_IRMCUSTOMER_FORM_CANCEL_CHECKIN'); ?></a>
+					<a href="<?php echo JRoute::_('index.php?option=com_xiveirm&task=irmcustomer.cancel'); ?>" class="btn btn-danger"><i class="icon-reply"></i> <?php echo JText::_('COM_XIVEIRM_CANCEL_ITEM'); ?></a>
 				</div>
 			</div>
 			<!-- ---------- ---------- ---------- ---------- ---------- BEGIN master-tab-pane-container ---------- ---------- ---------- ---------- ---------- -->
@@ -313,113 +314,18 @@ $dispatcher = JDispatcher::getInstance();
 		<!-- ---------- ---------- ---------- ---------- ---------- END MASTER_TAP_PANE_PLUGINSTYLED ---------- ---------- ---------- ---------- ---------- -->
 
 	</form>
+	<form id="form-irmcustomer-cica">
+		<input type="hidden" name="cica[id]" value="<?php echo isset($this->item->id) ? $this->item->id : '0'; ?>" />
+		<?php echo JHtml::_('form.token'); ?>
+	</form>
 </div>
 
+<script>
+	// Prevent submit on enter (keycode 13) event in form fields
+	jQuery("#form-irmcustomer").bind('keypress keydown keyup', function(e) {
+		if(e.keyCode == 13) { e.preventDefault(); }
+	});
 
-
-
-
-
-<?php if($this->item->id): ?>
-	<script>
-	// XAP-TODO: Have to set more functions to the edit form, such as a DB-checkout on activate and checkin on save or check in on deactivate !!!!
-		jQuery(function() {
-			// Prevent submit on enter (keycode 13) event in form fields
-			$("#form-irmcustomer").bind('keypress keydown keyup', function(e) {
-				if(e.keyCode == 13) { e.preventDefault(); }
-			});
-
-			// Enable edit event button
-			$("#form-irmcustomer .input-control").attr("readonly", true);
-			var inp = $('.input-control').get(0);
-			var classInp = $('#input-control-button').get(0);
-	
-			$('#id-disable-check').on('click', function() {
-				if(inp.hasAttribute('readonly')) {
-					$("#form-irmcustomer .input-control").attr("readonly", false);
-					classInp.removeAttribute('class', 'red');
-					classInp.setAttribute('class' , 'control-label green');
-					$("#form-buttons").removeClass("hidden");
-				} else {
-					$("#form-irmcustomer .input-control").attr("readonly", true);
-					classInp.removeAttribute('class', 'green');
-					classInp.setAttribute('class' , 'control-label red');
-					$("#form-buttons").addClass("hidden");
-				}
-				if ($("#checkout-info").is(":hidden")) {
-					setTimeout(function () {
-							$("#checkout-info").slideDown("slow");
-					}, 2000)
-				} else {
-					$("#checkout-info").slideUp("slow");
-					setTimeout(function () {
-						$.gritter.add({
-							title: 'Successfully checked-in',
-							text: 'You have successfully checked-in this contact, so other user can edit now',
-							class_name: 'gritter-success'
-						});
-					}, 2000)
-				}
-			});
-		});
-	</script>
-<?php endif; ?>
-
-
-
-
-
-
-
-
-		<script>
-			jQuery(function(){
-				$("#form-irmcustomer").submit(function(e){
-					e.preventDefault();
-
-					$("#loading-btn-recall").addClass("btn-warning");
-					$("#loading-btn-recall").button("loading");
-
-					$.post('index.php?option=com_xiveirm&task=api.ajaxsave', $("#form-irmcustomer").serialize(),
-					function(data){
-						if(data.apiReturnCode === 'SAVED'){
-							$.gritter.add({
-								title: 'Successfully saved',
-								text: 'You have successfully saved all items for the customer <?php echo $this->item->first_name . ' ' . $this->item->last_name; ?>',
-								icon: 'icon-check',
-								class_name: 'alert-success'
-							});
-							$("#customer_cid").val(data.apiReturnId);
-
-							$("#loading-btn-recall").removeClass("btn-warning");
-							$("#loading-btn-recall").button("complete");
-							$("#loading-btn-recall").button("reset");
-						} else if(data.apiReturnCode === 'UPDATED'){
-							$.gritter.add({
-								title: 'Successfully updated',
-								text: 'You have successfully saved all items for the customer <?php echo $this->item->first_name . ' ' . $this->item->last_name; ?>',
-								icon: 'icon-globe',
-								class_name: 'alert-info'
-							});
-							$("#loading-btn-recall").removeClass("btn-warning");
-							$("#loading-btn-recall").button("complete");
-							$("#loading-btn-recall").button("reset");
-						} else {
-							$.gritter.add({
-								title: 'An error occured',
-								text: 'An error occured while trying to save or update. <br><br>Error code: ' + data.apiReturnCode + '<br><br>error message: ' + data.apiReturnMessage + '<br><br>If this error is persistant, please contact the support immediately with the given error!',
-								icon: 'icon-warning-sign',
-								sticky: true,
-								class_name: 'alert-error'
-							});
-							$("#loading-btn-recall").removeClass("btn-warning");
-							$("#loading-btn-recall").button("error");
-							$("#loading-btn-recall").addClass("btn-danger");
-						}
-					}, "json");
-				});
-			});
-<?php
 	/*
 	 * Returns from API in json format
 	 * example {"apiReturnCode":"SAVED","apiReturnRowId":"173","apiReturnMessage":"Successfully saved"}
@@ -427,5 +333,164 @@ $dispatcher = JDispatcher::getInstance();
 	 * apiReturnCode could be: SAVED, UPDATED or an Error Number ie. 666
 	 * apiReturnMessage: returns a informal message, should be used for debugging and not in production use. returns the database or php errors
 	 */
-?>
-		</script>
+	jQuery(function(){
+		<?php if($checked_out): ?>
+		// show a message that the user is checked out
+		if ($("#checkout-info").is(":hidden")) {
+			setTimeout(function () {
+				jQuery("#checkout-info").slideDown("slow");
+			}, 2000)
+		}
+		<?php endif; ?>
+
+		$("#form-irmcustomer").submit(function(e){
+			e.preventDefault();
+
+			$("#loading-btn-save").addClass("btn-warning");
+			$("#loading-btn-save").button("loading");
+
+			$.post('index.php?option=com_xiveirm&task=api.ajaxsave', $("#form-irmcustomer").serialize(),
+			function(data){
+				if(data.apiReturnCode === 'SAVED'){
+					$.gritter.add({
+						title: 'Successfully saved',
+						text: 'You have successfully saved all items for the customer <?php echo $this->item->first_name . ' ' . $this->item->last_name; ?>',
+						icon: 'icon-check',
+						class_name: 'alert-success'
+					});
+					$("#customer_cid").val(data.apiReturnId);
+
+					$("#loading-btn-save").removeClass("btn-warning");
+					$("#loading-btn-save").button("complete");
+					$("#loading-btn-save").button("reset");
+				} else if(data.apiReturnCode === 'UPDATED') {
+					$.gritter.add({
+						title: 'Successfully updated',
+						text: 'You have successfully saved all items for the customer <?php echo $this->item->first_name . ' ' . $this->item->last_name; ?>',
+						icon: 'icon-globe',
+						class_name: 'alert-info'
+					});
+					$("#loading-btn-save").removeClass("btn-warning");
+					$("#loading-btn-save").button("complete");
+					$("#loading-btn-save").button("reset");
+				} else {
+					$.gritter.add({
+						title: 'An error occured',
+						text: 'An error occured while trying to save or update. <br><br>Error code: ' + data.apiReturnCode + '<br><br>error message: ' + data.apiReturnMessage + '<br><br>If this error is persistant, please contact the support immediately with the given error!',
+						icon: 'icon-warning-sign',
+						sticky: true,
+						class_name: 'alert-error'
+					});
+					$("#loading-btn-save").removeClass("btn-warning");
+					$("#loading-btn-save").button("error");
+					$("#loading-btn-save").addClass("btn-danger");
+				}
+			}, "json");
+		});
+	});
+
+	<?php if($this->item->id): ?>
+	/*
+	 *
+	 *
+	 * Item exist and we set now all input fields to readonly. Hit the edit button to remove all and check out the item!
+	 * Note:	that we have added the disabled attribute to all .btn classes in widget boxes. May we do not need them anymore,
+	 * 		because we have a function that prevent to leave the form if anything has changed!
+	 *
+	 */
+	jQuery("#form-irmcustomer .input-control").attr("readonly", true);
+
+	// XAP-TODO: Have to set more functions to the edit form, such as a DB-checkout on activate and checkin on save or check in on deactivate !!!!
+	function enableForm() {
+		var inp = $('.input-control').get(0);
+		var editBtn = $('.edit-form-button').get(0);
+
+		$("#loading-btn-edit").addClass("btn-warning");
+		$("#loading-btn-edit").button("loading");
+
+		jQuery.post('index.php?option=com_xiveirm&task=api.ajaxcheckout', $("#form-irmcustomer-cica").serialize(),
+			function(data){
+				if(data.apiReturnCode === 'OUT'){
+					$.gritter.add({
+						title: '<?php echo JText::_('COM_XIVEIRM_IRMCUSTOMER_FORM_CHECKED_OUT_INFO_TITLE'); ?>',
+						text: '<?php echo JText::sprintf('COM_XIVEIRM_IRMCUSTOMER_FORM_CHECKED_OUT_INFO_BODY', $full_name); ?>',
+						icon: 'icon-signout',
+						class_name: 'alert-warning'
+					});
+
+					// Remove all readonly fields if we got an "OUT" response from the api
+					if(inp.hasAttribute('readonly')) {
+						$("#form-irmcustomer .input-control").attr("readonly", false);
+						editBtn.setAttribute("class", "hidden");
+						$("#form-buttons").removeClass("hidden");
+						$(".widget-box .btn").attr("disabled", true);
+					}
+				} else {
+					$.gritter.add({
+						title: 'An error occured',
+						text: 'An error occured while trying to check out for editing. <br><br>Error code: ' + data.apiReturnCode + '<br><br>error message: ' + data.apiReturnMessage + '<br><br>If this error is persistant, please contact the support immediately with the given error!',
+						icon: 'icon-warning-sign',
+						sticky: true,
+						class_name: 'alert-error'
+					});
+
+					$("#loading-btn-edit").removeClass("btn-warning");
+					$("#loading-btn-edit").button("error");
+					$("#loading-btn-edit").addClass("btn-danger");
+				}
+			},
+		"json");
+	}
+	<?php endif; ?>
+
+	/*
+	 *
+	 *
+	 * Prevent to leave this site, if anything in the form has changed and is not saved at present!
+	 *
+	 *
+	 */
+	var catcher = function() {
+		var changed = false;
+
+		$('form').each(function() {
+			if ($(this).data('initialForm') != $(this).serialize()) {
+				changed = true;
+				$(this).addClass('changed');
+			} else {
+				$(this).removeClass('changed');
+			}
+		});
+
+		if (changed) {
+			return '<?php echo JText::_('COM_XIVEIRM_PREVENT_LEAVE_SITE'); ?>';
+		}
+	};
+
+	jQuery(function() {
+		$('form').each(function() {
+			$(this).data('initialForm', $(this).serialize());
+		}).submit(function(e) {
+			var formEl = this;
+			var changed = false;
+
+			$('form').each(function() {
+				if (this != formEl && $(this).data('initialForm') != $(this).serialize()) {
+					changed = true;
+					$(this).addClass('changed');
+				} else {
+					$(this).removeClass('changed');
+				}
+			});
+
+			// If we have 2 or more forms on this page - Be careful with hidden forms!!
+			if (changed && !confirm('<?php echo JText::_('COM_XIVEIRM_PREVENT_FORM_SUBMISSION'); ?>')) {
+				e.preventDefault();
+			} else {
+				$(window).unbind('beforeunload', catcher);
+			}
+		});
+
+		$(window).bind('beforeunload', catcher);
+	});
+</script>
