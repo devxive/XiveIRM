cdf3f636f54cc793b0f34fab100498621cb97a77
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-06-15 17:35:33 +0200

    disabled unused methods for later removing

--- source/package/components/com_xiveirm/site/controllers/irmcustomer.php
+++ source/package/components/com_xiveirm/site/controllers/irmcustomer.php
@@ -39,188 +39,188 @@ class XiveirmControllerIrmcustomer extends XiveirmController
 
 		// Check out the item
 		if ($editId) {
-            $model->checkout($editId);
+//            $model->checkout($editId);
 		}
 
 		// Check in the previous user.
 		if ($previousId) {
-            $model->checkin($previousId);
+//            $model->checkin($previousId);
 		}
 
 		// Redirect to the edit screen.
 		$this->setRedirect(JRoute::_('index.php?option=com_xiveirm&view=irmcustomerform&layout=edit', false));
 	}
 
-	/**
-	 * Method to save a user's profile data.
-	 *
-	 * @return	void
-	 * @since	1.6
-	 */
-	public function save()
-	{
-		// Check for request forgeries.
-		JSession::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
-
-		// Initialise variables.
-		$app	= JFactory::getApplication();
-		$model = $this->getModel('Irmcustomer', 'XiveirmModel');
-
-		// Get the user data.
-		$data = JFactory::getApplication()->input->get('jform', array(), 'array');
-
-		// Validate the posted data.
-		$form = $model->getForm();
-		if (!$form) {
-			JError::raiseError(500, $model->getError());
-			return false;
-		}
-
-		// Validate the posted data.
-		$data = $model->validate($form, $data);
-
-		// Check for errors.
-		if ($data === false) {
-			// Get the validation messages.
-			$errors	= $model->getErrors();
-
-			// Push up to three validation messages out to the user.
-			for ($i = 0, $n = count($errors); $i < $n && $i < 3; $i++) {
-				if ($errors[$i] instanceof Exception) {
-					$app->enqueueMessage($errors[$i]->getMessage(), 'warning');
-				} else {
-					$app->enqueueMessage($errors[$i], 'warning');
-				}
-			}
-
-			// Save the data in the session.
-			$app->setUserState('com_xiveirm.edit.irmcustomer.data', JRequest::getVar('jform'),array());
-
-			// Redirect back to the edit screen.
-			$id = (int) $app->getUserState('com_xiveirm.edit.irmcustomer.id');
-			$this->setRedirect(JRoute::_('index.php?option=com_xiveirm&view=irmcustomer&layout=edit&id='.$id, false));
-			return false;
-		}
-
-		// Attempt to save the data.
-		$return	= $model->save($data);
-
-		// Check for errors.
-		if ($return === false) {
-			// Save the data in the session.
-			$app->setUserState('com_xiveirm.edit.irmcustomer.data', $data);
-
-			// Redirect back to the edit screen.
-			$id = (int)$app->getUserState('com_xiveirm.edit.irmcustomer.id');
-			$this->setMessage(JText::sprintf('Save failed', $model->getError()), 'warning');
-			$this->setRedirect(JRoute::_('index.php?option=com_xiveirm&view=irmcustomer&layout=edit&id='.$id, false));
-			return false;
-		}
-
-            
-        // Check in the profile.
-        if ($return) {
-            $model->checkin($return);
-        }
-        
-        // Clear the profile id from the session.
-        $app->setUserState('com_xiveirm.edit.irmcustomer.id', null);
-
-        // Redirect to the list screen.
-        $this->setMessage(JText::_('COM_XIVEIRM_ITEM_SAVED_SUCCESSFULLY'));
-        $menu = & JSite::getMenu();
-        $item = $menu->getActive();
-        $this->setRedirect(JRoute::_($item->link, false));
-
-		// Flush the data from the session.
-		$app->setUserState('com_xiveirm.edit.irmcustomer.data', null);
-	}
+//	/**
+//	 * Method to save a user's profile data.
+//	 *
+//	 * @return	void
+//	 * @since	1.6
+//	 */
+//	public function save()
+//	{
+//		// Check for request forgeries.
+//		JSession::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
+//
+//		// Initialise variables.
+//		$app	= JFactory::getApplication();
+//		$model = $this->getModel('Irmcustomer', 'XiveirmModel');
+//
+//		// Get the user data.
+//		$data = JFactory::getApplication()->input->get('jform', array(), 'array');
+//
+//		// Validate the posted data.
+//		$form = $model->getForm();
+//		if (!$form) {
+//			JError::raiseError(500, $model->getError());
+//			return false;
+//		}
+//
+//		// Validate the posted data.
+//		$data = $model->validate($form, $data);
+//
+//		// Check for errors.
+//		if ($data === false) {
+//			// Get the validation messages.
+//			$errors	= $model->getErrors();
+//
+//			// Push up to three validation messages out to the user.
+//			for ($i = 0, $n = count($errors); $i < $n && $i < 3; $i++) {
+//				if ($errors[$i] instanceof Exception) {
+//					$app->enqueueMessage($errors[$i]->getMessage(), 'warning');
+//				} else {
+//					$app->enqueueMessage($errors[$i], 'warning');
+//				}
+//			}
+//
+//			// Save the data in the session.
+//			$app->setUserState('com_xiveirm.edit.irmcustomer.data', JRequest::getVar('jform'),array());
+//
+//			// Redirect back to the edit screen.
+//			$id = (int) $app->getUserState('com_xiveirm.edit.irmcustomer.id');
+//			$this->setRedirect(JRoute::_('index.php?option=com_xiveirm&view=irmcustomer&layout=edit&id='.$id, false));
+//			return false;
+//		}
+//
+//		// Attempt to save the data.
+//		$return	= $model->save($data);
+//
+//		// Check for errors.
+//		if ($return === false) {
+//			// Save the data in the session.
+//			$app->setUserState('com_xiveirm.edit.irmcustomer.data', $data);
+//
+//			// Redirect back to the edit screen.
+//			$id = (int)$app->getUserState('com_xiveirm.edit.irmcustomer.id');
+//			$this->setMessage(JText::sprintf('Save failed', $model->getError()), 'warning');
+//			$this->setRedirect(JRoute::_('index.php?option=com_xiveirm&view=irmcustomer&layout=edit&id='.$id, false));
+//			return false;
+//		}
+//
+//            
+//        // Check in the profile.
+//        if ($return) {
+//            $model->checkin($return);
+//        }
+//        
+//        // Clear the profile id from the session.
+//        $app->setUserState('com_xiveirm.edit.irmcustomer.id', null);
+//
+//        // Redirect to the list screen.
+//        $this->setMessage(JText::_('COM_XIVEIRM_ITEM_SAVED_SUCCESSFULLY'));
+//        $menu = & JSite::getMenu();
+//        $item = $menu->getActive();
+//        $this->setRedirect(JRoute::_($item->link, false));
+//
+//		// Flush the data from the session.
+//		$app->setUserState('com_xiveirm.edit.irmcustomer.data', null);
+//	}
     
     
-    function cancel() {
-		$menu = & JSite::getMenu();
-        $item = $menu->getActive();
-        $this->setRedirect(JRoute::_($item->link, false));
-    }
+//	function cancel() {
+//		$menu = & JSite::getMenu();
+//		$item = $menu->getActive();
+//		$this->setRedirect(JRoute::_($item->link, false));
+//	}
     
-	public function remove()
-	{
-		// Check for request forgeries.
-		JSession::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
-
-		// Initialise variables.
-		$app	= JFactory::getApplication();
-		$model = $this->getModel('Irmcustomer', 'XiveirmModel');
-
-		// Get the user data.
-		$data = JFactory::getApplication()->input->get('jform', array(), 'array');
-
-		// Validate the posted data.
-		$form = $model->getForm();
-		if (!$form) {
-			JError::raiseError(500, $model->getError());
-			return false;
-		}
-
-		// Validate the posted data.
-		$data = $model->validate($form, $data);
-
-		// Check for errors.
-		if ($data === false) {
-			// Get the validation messages.
-			$errors	= $model->getErrors();
-
-			// Push up to three validation messages out to the user.
-			for ($i = 0, $n = count($errors); $i < $n && $i < 3; $i++) {
-				if ($errors[$i] instanceof Exception) {
-					$app->enqueueMessage($errors[$i]->getMessage(), 'warning');
-				} else {
-					$app->enqueueMessage($errors[$i], 'warning');
-				}
-			}
-
-			// Save the data in the session.
-			$app->setUserState('com_xiveirm.edit.irmcustomer.data', $data);
-
-			// Redirect back to the edit screen.
-			$id = (int) $app->getUserState('com_xiveirm.edit.irmcustomer.id');
-			$this->setRedirect(JRoute::_('index.php?option=com_xiveirm&view=irmcustomer&layout=edit&id='.$id, false));
-			return false;
-		}
-
-		// Attempt to save the data.
-		$return	= $model->delete($data);
-
-		// Check for errors.
-		if ($return === false) {
-			// Save the data in the session.
-			$app->setUserState('com_xiveirm.edit.irmcustomer.data', $data);
-
-			// Redirect back to the edit screen.
-			$id = (int)$app->getUserState('com_xiveirm.edit.irmcustomer.id');
-			$this->setMessage(JText::sprintf('Delete failed', $model->getError()), 'warning');
-			$this->setRedirect(JRoute::_('index.php?option=com_xiveirm&view=irmcustomer&layout=edit&id='.$id, false));
-			return false;
-		}
-
-            
-        // Check in the profile.
-        if ($return) {
-            $model->checkin($return);
-        }
-        
-        // Clear the profile id from the session.
-        $app->setUserState('com_xiveirm.edit.irmcustomer.id', null);
-
-        // Redirect to the list screen.
-        $this->setMessage(JText::_('COM_XIVEIRM_ITEM_DELETED_SUCCESSFULLY'));
-        $menu = & JSite::getMenu();
-        $item = $menu->getActive();
-        $this->setRedirect(JRoute::_($item->link, false));
-
-		// Flush the data from the session.
-		$app->setUserState('com_xiveirm.edit.irmcustomer.data', null);
-	}
+//	public function remove()
+//	{
+//		// Check for request forgeries.
+//		JSession::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
+//
+//		// Initialise variables.
+//		$app	= JFactory::getApplication();
+//		$model = $this->getModel('Irmcustomer', 'XiveirmModel');
+//
+//		// Get the user data.
+//		$data = JFactory::getApplication()->input->get('jform', array(), 'array');
+//
+//		// Validate the posted data.
+//		$form = $model->getForm();
+//		if (!$form) {
+//			JError::raiseError(500, $model->getError());
+//			return false;
+//		}
+//
+//		// Validate the posted data.
+//		$data = $model->validate($form, $data);
+//
+//		// Check for errors.
+//		if ($data === false) {
+//			// Get the validation messages.
+//			$errors	= $model->getErrors();
+//
+//			// Push up to three validation messages out to the user.
+//			for ($i = 0, $n = count($errors); $i < $n && $i < 3; $i++) {
+//				if ($errors[$i] instanceof Exception) {
+//					$app->enqueueMessage($errors[$i]->getMessage(), 'warning');
+//				} else {
+//					$app->enqueueMessage($errors[$i], 'warning');
+//				}
+//			}
+//
+//			// Save the data in the session.
+//			$app->setUserState('com_xiveirm.edit.irmcustomer.data', $data);
+//
+//			// Redirect back to the edit screen.
+//			$id = (int) $app->getUserState('com_xiveirm.edit.irmcustomer.id');
+//			$this->setRedirect(JRoute::_('index.php?option=com_xiveirm&view=irmcustomer&layout=edit&id='.$id, false));
+//			return false;
+//		}
+//
+//		// Attempt to save the data.
+//		$return	= $model->delete($data);
+//
+//		// Check for errors.
+//		if ($return === false) {
+//			// Save the data in the session.
+//			$app->setUserState('com_xiveirm.edit.irmcustomer.data', $data);
+//
+//			// Redirect back to the edit screen.
+//			$id = (int)$app->getUserState('com_xiveirm.edit.irmcustomer.id');
+//			$this->setMessage(JText::sprintf('Delete failed', $model->getError()), 'warning');
+//			$this->setRedirect(JRoute::_('index.php?option=com_xiveirm&view=irmcustomer&layout=edit&id='.$id, false));
+//			return false;
+//		}
+//
+//            
+//        // Check in the profile.
+//        if ($return) {
+//            $model->checkin($return);
+//        }
+//        
+//        // Clear the profile id from the session.
+//        $app->setUserState('com_xiveirm.edit.irmcustomer.id', null);
+//
+//        // Redirect to the list screen.
+//        $this->setMessage(JText::_('COM_XIVEIRM_ITEM_DELETED_SUCCESSFULLY'));
+//        $menu = & JSite::getMenu();
+//        $item = $menu->getActive();
+//        $this->setRedirect(JRoute::_($item->link, false));
+//
+//		// Flush the data from the session.
+//		$app->setUserState('com_xiveirm.edit.irmcustomer.data', null);
+//	}
     
     
 }
