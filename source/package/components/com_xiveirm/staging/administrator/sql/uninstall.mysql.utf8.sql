1eb59bb66893d03b25d45c6ebe86b72346ad6bf4
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-06-17 23:39:07 +0200

    removed staging

--- source/package/components/com_xiveirm/staging/administrator/sql/uninstall.mysql.utf8.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS `#__xiveirm_contacts`;
-DROP TABLE IF EXISTS `#__xiveirm_contact_tabappvalues`;
-DROP TABLE IF EXISTS `#__xiveirm_options`;
-DROP TABLE IF EXISTS `#__xiveirm_option_categories`;
-DROP TABLE IF EXISTS `#__xiveirm_tabapps`;
