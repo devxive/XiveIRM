15d7c1b6bc1cb0e4fb63a6913a74bde28cd3202e
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-06-28 23:13:34 +0200

    renamed module templates

--- source/package/modules/mod_xiveirm_alertbox_mytasks/tmpl/default.php
+++ source/package/modules/mod_xiveirm_alertbox_mytasks/tmpl/default.php
@@ -8,66 +8,62 @@
  */
 
 defined('_JEXEC') or die;
-$icon1 = array('icon' => $params->get('icon1_icon'), 'color' => $params->get('icon1_color'), 'title' => $params->get('icon1_title'), 'link' => $params->get('icon1_link'));
-$icon2 = array('icon' => $params->get('icon2_icon'), 'color' => $params->get('icon2_color'), 'title' => $params->get('icon2_title'), 'link' => $params->get('icon2_link'));
-$icon3 = array('icon' => $params->get('icon3_icon'), 'color' => $params->get('icon3_color'), 'title' => $params->get('icon3_title'), 'link' => $params->get('icon3_link'));
-$icon4 = array('icon' => $params->get('icon4_icon'), 'color' => $params->get('icon4_color'), 'title' => $params->get('icon4_title'), 'link' => $params->get('icon4_link'));
 ?>
-					<li class="grey dark no-border margin-1">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-							<i class="icon-tasks icon-animated-wrench icon-only"></i>
-							<span class="badge">4</span>
-						</a>
-						<ul class="pull-right dropdown-navbar dropdown-menu dropdown-caret dropdown-closer">
-							<li class="nav-header">
-								<i class="icon-ok"></i> 4 Aufgaben zu erledigen
-							</li>
-							
-							<li>
-								<a href="#">
-									<div class="clearfix">
-										<span class="pull-left">Keine Geburtsdaten</span>
-										<span class="pull-right">65%</span>
-									</div>
-									<div class="progress progress-mini"><div class="bar" style="width:65%"></div></div>
-								</a>
-							</li>
-							
-							<li>
-								<a href="#">
-									<div class="clearfix">
-										<span class="pull-left">Adressen unvollst&auml;ndig</span>
-										<span class="pull-right">35%</span>
-									</div>
-									<div class="progress progress-mini progress-danger"><div class="bar" style="width:35%"></div></div>
-								</a>
-							</li>
-							
-							<li>
-								<a href="#">
-									<div class="clearfix">
-										<span class="pull-left">Abrechnungsrelevanz</span>
-										<span class="pull-right">15%</span>
-									</div>
-									<div class="progress progress-mini progress-warning"><div class="bar" style="width:15%"></div></div>
-								</a>
-							</li>
-							
-							<li>
-								<a href="#">
-									<div class="clearfix">
-										<span class="pull-left">Vorplanung</span>
-										<span class="pull-right">90%</span>
-									</div>
-									<div class="progress progress-mini progress-success progress-striped active"><div class="bar" style="width:90%"></div></div>
-								</a>
-							</li>
-							
-							<li>
-								<a href="#">
-									Aufgabendetails anzeigen
-									<i class="icon-arrow-right"></i>
-								</a>
-							</li>
-						</ul>
-					</li>
+<li class="grey dark no-border margin-1">
+	<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+		<i class="icon-tasks icon-animated-wrench icon-only"></i>
+		<span class="badge">4</span>
+	</a>
+	<ul class="pull-right dropdown-navbar dropdown-menu dropdown-caret dropdown-closer">
+		<li class="nav-header">
+			<i class="icon-ok"></i> 4 Aufgaben zu erledigen
+		</li>
+
+		<li>
+			<a href="#">
+				<div class="clearfix">
+					<span class="pull-left">Keine Geburtsdaten</span>
+					<span class="pull-right">65%</span>
+				</div>
+				<div class="progress progress-mini"><div class="bar" style="width:65%"></div></div>
+			</a>
+		</li>
+
+		<li>
+			<a href="#">
+				<div class="clearfix">
+					<span class="pull-left">Adressen unvollst&auml;ndig</span>
+					<span class="pull-right">35%</span>
+				</div>
+				<div class="progress progress-mini progress-danger"><div class="bar" style="width:35%"></div></div>
+			</a>
+		</li>
+
+		<li>
+			<a href="#">
+				<div class="clearfix">
+					<span class="pull-left">Abrechnungsrelevanz</span>
+					<span class="pull-right">15%</span>
+				</div>
+				<div class="progress progress-mini progress-warning"><div class="bar" style="width:15%"></div></div>
+			</a>
+		</li>
+
+		<li>
+			<a href="#">
+				<div class="clearfix">
+					<span class="pull-left">Vorplanung</span>
+					<span class="pull-right">90%</span>
+				</div>
+				<div class="progress progress-mini progress-success progress-striped active"><div class="bar" style="width:90%"></div></div>
+			</a>
+		</li>
+
+		<li>
+			<a href="#">
+				Aufgabendetails anzeigen
+				<i class="icon-arrow-right"></i>
+			</a>
+		</li>
+	</ul>
+</li>
