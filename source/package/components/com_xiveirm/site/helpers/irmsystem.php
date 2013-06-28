7fb60eba53504b4a36cba91c845545e866b6e4ac
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-06-28 23:43:53 +0200

    added flagIt method

--- source/package/components/com_xiveirm/site/helpers/irmsystem.php
+++ source/package/components/com_xiveirm/site/helpers/irmsystem.php
@@ -300,4 +300,78 @@ class IRMSystem
 		// NOTE: This is atypical to the normal viewing access levels and we need this for handle all client related stuff with this permissions/assets
 		return $results;
 	}
+
+	/*
+	 * Global Checkout Method to flag a contact
+	 * return true if success, else return false
+	 * @id		int		The db row id from the contact
+	 */
+	public function flagIt($id, $type = false)
+	{
+		if($id == 0 && (int) $id) {
+			return false;
+		}
+
+		// Init database object.
+		$db = JFactory::getDBO();
+		$query = $db->getQuery(true);
+
+		$dbTable = '#__xiveirm_flags';
+		$item = 'contacts.' . $id;
+
+		// Check if we have already a flag
+		$query
+			->select('flag')
+			->from($db->quoteName($dbTable))
+			->where('item = ' . $db->quote($item) . '');
+		$db->setQuery($query);
+		$result = $db->loadResult();
+
+		// If we only check the flag
+		if($type == 'check') {
+			return $result;
+		}
+
+		if($result) {
+		// DELETE FLAG
+			$query = 'DELETE FROM #__xiveirm_flags WHERE item = '.$db->quote($item).'';
+			$action = false;
+		} else {
+		// CREATE FLAG
+			// Set the fields
+			$columns = array('item', 'flag');
+			$values = array($db->quote($item), $db->quote(1));
+
+			$query
+				->insert($db->quoteName($dbTable))
+				->columns($db->quoteName($columns))
+				->values(implode(',', $values));
+			$action = true;
+		}
+
+		$db->setQuery($query);
+
+		// Try to store or get the error code for debugging
+		try
+		{
+			if(!$db->execute()) {
+				throw new Exception($db->getErrorMsg());
+			} else {
+				// Get the contact and prepare the return
+				$query = $db->getQuery(true);
+				$query
+					->select(array('company', 'last_name', 'first_name'))
+					->from($db->quoteName('#__xiveirm_contacts'))
+					->where('id = ' . $db->quote($id) . '');
+				$db->setQuery($query);
+				$result = $db->loadObject();
+				$result->action = $action;
+
+				return $result;
+			}
+		} catch (Exception $e) {
+			JError::raiseError(500, $e->getMessage());
+			return false;
+		}
+	}
 }
