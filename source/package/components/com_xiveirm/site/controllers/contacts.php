7756a230ef7876d3eac7ec153e639670ee73b91f
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-07-10 16:06:55 +0200

    added filter options

--- source/package/components/com_xiveirm/site/controllers/contacts.php
+++ source/package/components/com_xiveirm/site/controllers/contacts.php
@@ -26,4 +26,34 @@ class XiveirmControllerContacts extends XiveirmController
 		$model = parent::getModel($name, $prefix, array('ignore_request' => true));
 		return $model;
 	}
+
+	/**
+	 * Function to set a filter.
+	 * What it does?
+	 * 		Get the filter from a form GET or POST or an url and set the vale in the userState object
+	 *		After that, redirects to the contacts list, where before the list is rendered, the model is called
+	 *		In the model we get the userState object and set the where clause for the filter
+	 *
+	 * @since	4.0
+	 */
+	public function filter($search_global = null, $search_catid = null, $search_task = null)
+	{
+		$app = JFactory::getApplication();
+
+		// Get and set the search filter for the filter query (in model)
+		$search = array();
+		if(!$search['global'] = $app->input->get('search_global', null, 'string')) { unset($search['global']); };
+		if(!$search['catid'] = $app->input->get('search_catid', null, 'string')) { unset($search['catid']); };
+		if(!$search['pdk'] = $app->input->get('search_pdk', null, 'string')) { unset($search['pdk']); };
+
+		$app->setUserState('com_xiveirm.contacts.filter', $search);
+
+		// Redirect to the list.
+		$this->setRedirect(JRoute::_('index.php?option=com_xiveirm', false));
+
+$userState = $app->getUserState('com_xiveirm.contacts');
+echo '<pre>';
+print_r($userState);
+echo '</pre>';
+	}
 }
--- source/package/components/com_xiveirm/site/models/contacts.php
+++ source/package/components/com_xiveirm/site/models/contacts.php
@@ -35,21 +35,43 @@ class XiveirmModelContacts extends JModelList {
      */
     protected function populateState($ordering = null, $direction = null) {
 
-        // Initialise variables.
-        $app = JFactory::getApplication();
+	// Initialise variables.
+	$app = JFactory::getApplication();
+	$session = JFactory::getSession();
 
-        // List state information
-        $limit = $app->getUserStateFromRequest('global.list.limit', 'limit', $app->getCfg('list_limit'));
-        $this->setState('list.limit', $limit);
+	// List state information
+	$limit = $app->getUserStateFromRequest('global.list.limit', 'limit', $app->getCfg('list_limit'));
+	$this->setState('list.limit', $limit);
 
-        $limitstart = JFactory::getApplication()->input->getInt('limitstart', 0);
-        $this->setState('list.start', $limitstart);
+	$limitstart = JFactory::getApplication()->input->getInt('limitstart', 0);
+	$this->setState('list.start', $limitstart);
 
-	$search = $this->getUserStateFromRequest($this->context.'.filter.search', 'filter_search');
-	$this->setState('filter.search', preg_replace('/\s+/', ' ', $search));
+	// Set state for client_id (usergroup)
+	if ($irmSession = $session->get('XiveIRMSystem')) {
+		$this->setState('filter.client_id', (int) $irmSession->client_id);
+	}
 
-        // List state information.
-        parent::populateState($ordering, $direction);
+	// Set the filter states
+	$filter = $app->getUserState('com_xiveirm.contacts.filter');
+
+	// Set state for global search phrase
+	$filter_global = isset($filter['global']) ? preg_replace('/\s+/', ' ', $filter['global']) : null;
+	$this->setState('filter.global', preg_replace('/\s+/', ' ', $filter_global));
+
+	// Set state for category search
+	$filter_catid = isset($filter['catid']) ? $filter['catid'] : null;
+	$this->setState('filter.catid', $filter_catid);
+
+	// Set state for task search
+	$filter_pdk = isset($filter['pdk']) ? $filter['pdk'] : null;
+	$this->setState('filter.pdk', $filter_pdk);
+
+	// Set state for global search by phrase
+//	$search = $this->getUserStateFromRequest($this->context.'.filter.search', 'filter_search');
+//	$this->setState('filter.search', preg_replace('/\s+/', ' ', $search));
+
+	// List state information.
+	parent::populateState($ordering, $direction);
     }
 
     /**
@@ -59,56 +81,81 @@ class XiveirmModelContacts extends JModelList {
      * @since	1.6
      */
     protected function getListQuery() {
-        // Create a new query object.
-        $db = $this->getDbo();
-        $query = $db->getQuery(true);
+	// Create a new query object.
+	$db = $this->getDbo();
+	$query = $db->getQuery(true);
 
-        // Select the required fields from the table.
-        $query->select(
-                $this->getState(
-                        'list.select', 'a.*'
-                )
-        );
+	// Select the required fields from the table.
+	$query->select(
+		$this->getState(
+			'list.select', 'a.*'
+		)
+	);
 
-        $query->from('`#__xiveirm_contacts` AS a');
+	$query->from('`#__xiveirm_contacts` AS a');
 
-        
-    // Join over the users for the checked out user.
-    $query->select('uc.name AS editor');
-    $query->join('LEFT', '#__users AS uc ON uc.id=a.checked_out');
-    
-		// Join over the created by field 'created_by'
-		$query->select('created_by.name AS created_by');
-		$query->join('LEFT', '#__users AS created_by ON created_by.id = a.created_by');
+	// Join over the users for the checked out user.
+	$query->select('uc.name AS editor');
+	$query->join('LEFT', '#__users AS uc ON uc.id=a.checked_out');
 
-		// Join over the category 'catid'
-		$query->select('catid.title AS catid_title');
-		$query->join('LEFT', '#__categories AS catid ON catid.id = a.catid');
+	// Join over the created by field 'created_by'
+	$query->select('created_by.name AS created_by');
+	$query->join('LEFT', '#__users AS created_by ON created_by.id = a.created_by');
 
-		// Join over the category 'gender'
-		$query->select('gender.title AS gender_title');
-		$query->join('LEFT', '#__categories AS gender ON gender.id = a.gender');
+	// Join over the category 'catid'
+	$query->select('catid.title AS catid_title');
+	$query->join('LEFT', '#__categories AS catid ON catid.id = a.catid');
 
-		// Filter by search in title
-		$search = $this->getState('filter.search');
-		if (!empty($search)) {
-			if (stripos($search, 'id:') === 0) {
-				$query->where('a.id = ' . (int) substr($search, 3));
-			} else if ($search === '09') {
-				$query->where('a.last_name NOT REGEXP \'^[[:alpha:]]\'');
-			} else {
-				$search = $db->Quote($db->escape($search, true) . '%');
-//				$query->where('( a.company LIKE '.$search.'  OR  a.last_name LIKE '.$search.'  OR  a.first_name LIKE '.$search.' )');
-				$query->where('a.last_name LIKE ' . $search);
-			}
+	// Join over the category 'gender'
+	$query->select('gender.title AS gender_title');
+	$query->join('LEFT', '#__categories AS gender ON gender.id = a.gender');
+
+	// Join over the flags
+	$query->select('flags.flag AS flagged');
+	$query->join('LEFT', '#__xiveirm_flags AS flags ON flags.item = CONCAT(\'contacts.\', a.id)');
+
+	// Set the client_id (usergroup)
+	if ($filter_client_id = $this->state->get("filter.client_id")) {
+		$query->where('a.client_id = ' . $filter_client_id . '');
+	}
+
+	// Filtering catid
+	if ($filter_catid = $this->state->get("filter.catid")) {
+		$query->where("a.catid = '".$filter_catid."'");
+	}
+
+	// Filtering by global search phrase
+	if ($filter_global = $this->state->get("filter.global")) {
+		$filter_global = $db->Quote($db->escape($filter_global, true) . '%');
+		$query->where('( a.company LIKE '.$filter_global.'  OR  a.last_name LIKE '.$filter_global.'  OR  a.first_name LIKE '.$filter_global.' )');
+//			$filter_global = $db->Quote($db->escape($filter_global, true));
+//			$query->where('a.id = ' . (int) substr($filter_global, 3));
+	}
+
+	// If the filter_pdk (pre defined key) is set, use the filter_pdk query
+	if ($filter_pdk = $this->state->get("filter.pdk")) {
+		if($filter_pdk == 'pdk_flagged') {
+			$query->where('flags.flag = 1');
+		} else if($filter_pdk == 'pdk_incomplete_address') {
+			$query->where('a.address_street = \'\' OR a.address_houseno = \'\' OR a.address_zip = \'\' OR a.address_city = \'\' OR a.address_country = \'\'');
+		} else if($filter_pdk == 'pdk_no_phone') {
+			$query->where('a.phone = \'\' AND a.mobile = \'\'');
+		} else if($filter_pdk == 'pdk_checked_out') {
+			$userId = JFactory::getUser()->id;
+			$query->where('a.checked_out = ' . $userId . '');
+		} else if($filter_pdk == 'pdk_special') {
+			$query->where('(a.last_name NOT REGEXP \'^[[:alpha:]]*$\' AND a.last_name != \'\') OR (a.first_name NOT REGEXP \'^[[:alpha:]]*$\' AND a.first_name != \'\') OR (a.company NOT REGEXP \'^[[:alpha:]]\' AND a.company != \'\')');
+		} else if($filter_pdk == 'pdk_not_in_country') {
+			$query->where('a.address_country NOT IN (\'Deutschland\', \'Germany\')');
+		} else if($filter_pdk == 'pdk_in_country') {
+			$query->where('a.address_country IN (\'Deutschland\', \'Germany\')');
+		} else if($filter_pdk == 'pdk_no_customer_id') {
+			$query->where('a.customer_id = \'\'');
+		} else {
+			// What? Nothing!
 		}
+	}
 
-		//Filtering catid
-		$filter_catid = $this->state->get("filter.catid");
-		if ($filter_catid) {
-			$query->where("a.catid = '".$filter_catid."'");
-		}
-
-        return $query;
+	return $query;
     }
 }
