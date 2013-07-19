755ad25aea2baca273a535b292c74c499e4029c2
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-07-19 16:56:37 +0200

    removed medtranscoord

--- source/package/components/com_medtranscoord/administrator/sql/uninstall.mysql.utf8.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS `#__medtranscoord_order`;
-DROP TABLE IF EXISTS `#__medtranscoord_order_add`;
-DROP TABLE IF EXISTS `#__medtranscoord_order_billing`;
-DROP TABLE IF EXISTS `#__medtranscoord_order_statistic`;
-DROP TABLE IF EXISTS `#__medtranscoord_selectlists`;
