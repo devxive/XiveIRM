75be2c619071e2c6d58a90a33e7cdad44c30289e
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-07-28 22:19:57 +0200

    prepare file desc for version 6.0.0

--- source/package/components/com_xiveirm/site/models/api.php
+++ source/package/components/com_xiveirm/site/models/api.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version     3.3.0
+ * @version     6.0.0
  * @package     com_xiveirm
  * @copyright   Copyright (C) 1997 - 2013 by devXive - research and development. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
