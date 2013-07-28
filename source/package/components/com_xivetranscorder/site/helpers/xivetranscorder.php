c1d72ddb7a639ee0acbe76290198ac6e3454d7d8
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-07-28 22:07:30 +0200

    added helper method getTransDevice, TransType, OrderType

--- source/package/components/com_xivetranscorder/site/helpers/xivetranscorder.php
+++ source/package/components/com_xivetranscorder/site/helpers/xivetranscorder.php
@@ -17,5 +17,113 @@ abstract class XivetranscorderHelper
 		return $result;
 	}
 
+	public function getTransDevice()
+	{
+		NHtmlJavaScript::setChosen('.chzn-select-trans', false, array('width' => '100%', 'disable_search' => true));
+
+		$options = IRMSystem::getListOptions('options', 'transdevice');
+
+		$html = '';
+		$html .= '<select name="transcorder[transdevice]" class="chzn-select-trans input-control" data-placeholder="' . JText::_('COM_XIVEIRM_SELECT_CATEGORY') . '" required>';
+			$html .= '<option value="">' . JText::_('COM_XIVETRANSCORDER_FORM_SELECT_TRANSPORT_DEVICE') . '</option>';
+			if($options->client) {
+				$html .= '<optgroup label="' . JText::sprintf('COM_XIVETRANSCORDER_FORM_SELECT_SPECIFIC', NItemHelper::getTitleById('usergroup', $xsession->client_id)) . '">';
+					foreach ($options->client as $key => $val) {
+						if($this->item->gender == $key) {
+							$html .= '<option value="' . $key . '" selected>' . JText::_($val) . '</option>';
+						} else {
+							$html .= '<option value="' . $key . '">' . JText::_($val) . '</option>';
+						}
+					}
+				$html .= '</optgroup>';
+			}
+			if($options->global) {
+				$html .= '<optgroup label="' . JText::_('COM_XIVEIRM_SELECT_GLOBAL') . '">';
+					foreach ($options->global as $key => $val) {
+						if($this->item->gender == $key) {
+							$html .= '<option value="' . $key . '" selected>' . JText::_($val) . '</option>';
+						} else {
+							$html .= '<option value="' . $key . '">' . JText::_($val) . '</option>';
+						}
+					}
+				$html .= '</optgroup>';
+			}
+		$html .= '</select>';
+
+		return $html;
+	}
+
+	public function getTransType()
+	{
+		NHtmlJavaScript::setChosen('.chzn-select-trans', false, array('width' => '100%', 'disable_search' => true));
+
+		$options = IRMSystem::getListOptions('options', 'transtype');
+
+		$html = '';
+		$html .= '<select name="transcorder[transtype]" class="chzn-select-trans input-control" data-placeholder="' . JText::_('COM_XIVEIRM_SELECT_CATEGORY') . '" required>';
+			$html .= '<option value="">' . JText::_('COM_XIVETRANSCORDER_FORM_SELECT_TRANSPORT_TYPE') . '</option>';
+			if($options->client) {
+				$html .= '<optgroup label="' . JText::sprintf('COM_XIVETRANSCORDER_FORM_SELECT_SPECIFIC', NItemHelper::getTitleById('usergroup', $xsession->client_id)) . '">';
+					foreach ($options->client as $key => $val) {
+						if($this->item->gender == $key) {
+							$html .= '<option value="' . $key . '" selected>' . JText::_($val) . '</option>';
+						} else {
+							$html .= '<option value="' . $key . '">' . JText::_($val) . '</option>';
+						}
+					}
+				$html .= '</optgroup>';
+			}
+			if($options->global) {
+				$html .= '<optgroup label="' . JText::_('COM_XIVEIRM_SELECT_GLOBAL') . '">';
+					foreach ($options->global as $key => $val) {
+						if($this->item->gender == $key) {
+							$html .= '<option value="' . $key . '" selected>' . JText::_($val) . '</option>';
+						} else {
+							$html .= '<option value="' . $key . '">' . JText::_($val) . '</option>';
+						}
+					}
+				$html .= '</optgroup>';
+			}
+		$html .= '</select>';
+	
+		return $html;
+	}
+
+	public function getOrderType()
+	{
+		NHtmlJavaScript::setChosen('.chzn-select-ordertype', false, array('width' => '100%', 'disable_search' => true));
+
+		$options = IRMSystem::getListOptions('options', 'ordertype');
+
+		$html = '';
+		$html .= '<select name="transcorder[transtype]" class="chzn-select-trans input-control" data-placeholder="' . JText::_('COM_XIVEIRM_SELECT_CATEGORY') . '" required>';
+			$html .= '<option value="">' . JText::_('COM_XIVETRANSCORDER_FORM_SELECT_ORDER_TYPE') . '</option>';
+			if($options->client) {
+				$html .= '<optgroup label="' . JText::sprintf('COM_XIVETRANSCORDER_FORM_SELECT_SPECIFIC', NItemHelper::getTitleById('usergroup', $xsession->client_id)) . '">';
+					foreach ($options->client as $key => $val) {
+						if($this->item->gender == $key) {
+							$html .= '<option value="' . $key . '" selected>' . JText::_($val) . '</option>';
+						} else {
+							$html .= '<option value="' . $key . '">' . JText::_($val) . '</option>';
+						}
+					}
+				$html .= '</optgroup>';
+			}
+			if($options->global) {
+				$html .= '<optgroup label="' . JText::_('COM_XIVEIRM_SELECT_GLOBAL') . '">';
+					foreach ($options->global as $key => $val) {
+						if($this->item->gender == $key) {
+							$html .= '<option value="' . $key . '" selected>' . JText::_($val) . '</option>';
+						} else {
+							$html .= '<option value="' . $key . '">' . JText::_($val) . '</option>';
+						}
+					}
+				$html .= '</optgroup>';
+			}
+		$html .= '</select>';
+
+		return $html;
+	}
+
 }
 
