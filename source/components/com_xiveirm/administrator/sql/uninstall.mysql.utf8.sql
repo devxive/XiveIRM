3a7fbd6331d2f33652854f1f0802ea1a0886428c
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-08-02 22:56:10 +0200

    added flags table to uninstall

--- source/components/com_xiveirm/administrator/sql/uninstall.mysql.utf8.sql
+++ source/components/com_xiveirm/administrator/sql/uninstall.mysql.utf8.sql
@@ -2,3 +2,4 @@ DROP TABLE IF EXISTS `#__xiveirm_contacts`;
 DROP TABLE IF EXISTS `#__xiveirm_contact_tabappvalues`;
 DROP TABLE IF EXISTS `#__xiveirm_options`;
 DROP TABLE IF EXISTS `#__xiveirm_tabapps`;
+DROP TABLE IF EXISTS `#__xiveirm_flags`;
