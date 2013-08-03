a1d1aed828d723bf403829687e6d253a830b181b
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-08-03 09:51:11 +0200

    modified setup, duplicate categories, bind() error

--- source/components/com_xiveirm/setup.php
+++ source/components/com_xiveirm/setup.php
@@ -13,47 +13,88 @@
  * @assetsLicense	devXive Proprietary Use License (http://www.devxive.com/license)
  *
  * @since		5.0
+ *
+ * NOTE: Normally installer will look for setup.php in component folder during component install. Usage of z_setup.php will prevent the execution during component installation,
+ *       remains directory consistency and will be included in the setup.php in the root folder of installation and executes in the postflight section of the install script.
  */
 
 // Set variables
 $message = array();
-$element = 'com_xivetranscorder';
-$parent_menu = 'com_xiveirm';
+
+// Set the include path for the table class
+JTable::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_xiveirm/tables');
 
 if ($package['installer']->getInstallType() == 'install') {
-	// Set the Parent Category for all categories in the TOCA App
-	$parentCategoryId = NFWTableCategory::store(array('extension' => 'com_xiveirm','title' => 'COM_XIVECONTACTS_PARENT_CATEGORY', 'alias' => 'com-xivecontacts'));
+	// Runs on install
 
-	//Set gender category
-	$genderId = NFWTableCategory::store(array('extension' => 'com_xiveirm', 'title' => 'COM_XIVETRANSCORDER_CATEGORY_GENDER', 'alias' => 'gender', 'parent_id' => $parentCategoryId));
+	// Restore assets from backup
+	if (NFWInstallerHelper::restoreAssets($element)) {
+		$message[] = '<i class="icon-ok"></i> Assets restored successfully';
+	}
 
-	// Set the gender option values
-	if($genderId) {
-		$rows[] = array('client_id' => 2, 'catid' => $genderId, 'opt_value' => 'unknown', 'opt_name' => 'COM_XIVETRANSCORDER_OPTION_GENDER_TRAIT_UNKNOWN', 'access' => 2);
-		$rows[] = array('client_id' => 2, 'catid' => $genderId, 'opt_value' => 'female', 'opt_name' => 'COM_XIVETRANSCORDER_OPTION_GENDER_TRAIT_FEMALE', 'access' => 2);
-		$rows[] = array('client_id' => 2, 'catid' => $genderId, 'opt_value' => 'male', 'opt_name' => 'COM_XIVETRANSCORDER_OPTION_GENDER_TRAIT_MALE', 'access' => 2);
-		$rows[] = array('client_id' => 2, 'catid' => $genderId, 'opt_value' => 'company', 'opt_name' => 'COM_XIVETRANSCORDER_OPTION_GENDER_TRAIT_COMPANY', 'access' => 2);
+	/*
+	 * Set the parent category for all categories for the XiveContacts App
+	 */
+	$parentCategoryId = NFWTableCategory::store(array('extension' => 'com_xiveirm','title' => 'COM_XIVEIRM_CATEGORY_PARENT_XIVECONTACTS', 'access' => 2, 'language' => '*', 'alias' => 'com-xivecontacts'));
+	if($parentCategoryId) {
+		$message[] = '<i class="icon-ok"></i> Set XiveContacts Core Category ... OK';
+	} else {
+		$message[] = '<i class="icon-cancel"></i> Set XiveContacts Core Category ... FAILED';
+	}
+
+	/*
+	 * Set gender category as a subcategory of the XiveContacts Core Category
+	 */
+	$genderCategoryId = NFWTableCategory::store(array('extension' => 'com_xiveirm', 'title' => 'COM_XIVEIRM_CATEGORY_XIVECONTACTS_GENDER', 'alias' => 'xivecontacts-gender', 'access' => 2, 'language' => '*', 'parent_id' => $parentCategoryId));
+	if($genderCategoryId) {
+		$message[] = '<i class="icon-ok"></i> Set gender category ... OK';
+
+		// Set the gender option values
+		$rows[] = array('client_id' => 2, 'catid' => $genderCategoryId, 'opt_value' => 'unknown', 'opt_name' => 'COM_XIVEIRM_XIVECONTACTS_GENDER_TRAIT_UNKNOWN', 'access' => 2);
+		$rows[] = array('client_id' => 2, 'catid' => $genderCategoryId, 'opt_value' => 'female', 'opt_name' => 'COM_XIVEIRM_XIVECONTACTS_GENDER_TRAIT_FEMALE', 'access' => 2);
+		$rows[] = array('client_id' => 2, 'catid' => $genderCategoryId, 'opt_value' => 'male', 'opt_name' => 'COM_XIVEIRM_XIVECONTACTS_GENDER_TRAIT_MALE', 'access' => 2);
+		$rows[] = array('client_id' => 2, 'catid' => $genderCategoryId, 'opt_value' => 'company', 'opt_name' => 'COM_XIVEIRM_XIVECONTACTS_GENDER_TRAIT_COMPANY', 'access' => 2);
 
 		$data = (object) $rows;
 		$return = NFWTableData::store( 'Option', 'XiveirmTable', $data);
-
 		if($return) {
 			$message[] = '<i class="icon-ok"></i> Form - Set gender values ... OK';
 		} else {
 			$message[] = '<i class="icon-cancel"></i> Form - Set gender values ... FAILED';
 		}
-
-		$message[] = '<i class="icon-ok"></i> Set gender category ... OK';
 	} else {
 		$message[] = '<i class="icon-cancel"></i> Set gender category ... FAILED';
 	}
 
-	// Set the core contacts categories
-	
-} else {
-}
+	/*
+	 * Set parent contacts category
+	 */
+	$contactsCategoryId = NFWTableCategory::store(array('extension' => 'com_xiveirm', 'title' => 'COM_XIVEIRM_CATEGORY_XIVECONTACTS_CONTACTS', 'alias' => 'xivecontacts-contacts', 'access' => 2, 'language' => '*', 'parent_id' => $parentCategoryId));
+	if($contactsCategoryId) {
+		$message[] = '<i class="icon-ok"></i> Set parent contacts category ... OK';
 
-$message[] = '<i class="icon-ok"></i> Removed temporary files';
+		// Set the contacts related categories
+		// Kunde
+		$cc1 = NFWTableCategory::store(array('extension' => 'com_xiveirm','title' => 'COM_XIVEIRM_CATEGORY_XIVECONTACTS_CONTACTS_CUSTOMER', 'alias' => 'contacts-customer', 'access' => 2, 'language' => '*', 'parent_id' => $contactsCategoryId));
+		// Lieferant
+		$cc2 = NFWTableCategory::store(array('extension' => 'com_xiveirm','title' => 'COM_XIVEIRM_CATEGORY_XIVECONTACTS_CONTACTS_SUPPLIER', 'alias' => 'contacts-supplier', 'access' => 2, 'language' => '*', 'parent_id' => $contactsCategoryId));
+		// Subunternehmer
+		$cc3 = NFWTableCategory::store(array('extension' => 'com_xiveirm','title' => 'COM_XIVEIRM_CATEGORY_XIVECONTACTS_CONTACTS_SUBCONTRACTOR', 'alias' => 'contacts-subcontractor', 'access' => 2, 'language' => '*', 'parent_id' => $contactsCategoryId));
+		// Interessent
+		$cc4 = NFWTableCategory::store(array('extension' => 'com_xiveirm','title' => 'COM_XIVEIRM_CATEGORY_XIVECONTACTS_CONTACTS_LEAD', 'alias' => 'contacts-lead', 'access' => 2, 'language' => '*', 'parent_id' => $contactsCategoryId));
+		// Behörde
+		$cc5 = NFWTableCategory::store(array('extension' => 'com_xiveirm','title' => 'COM_XIVEIRM_CATEGORY_XIVECONTACTS_CONTACTS_AGENCY', 'alias' => 'contacts-agency', 'access' => 2, 'language' => '*', 'parent_id' => $contactsCategoryId));
+		if ($cc1 && $cc2 && $cc3 && $cc4 && $cc5 ) {
+			$message[] = '<i class="icon-ok"></i> Set contacts related categories ... OK';
+		} else {
+			$message[] = '<i class="icon-cancel"></i> Set contacts related categories ... FAILED!';
+		}
+	} else {
+		$message[] = '<i class="icon-cancel"></i> Set parent contacts category ... FAILED';
+	}
+} else {
+	// Runs on update
+}
 
 if (!empty($message)) {
 	return $message;
--- source/components/com_xivetranscorder/setup.php
+++ source/components/com_xivetranscorder/setup.php
@@ -13,6 +13,9 @@
  * @assetsLicense	devXive Proprietary Use License (http://www.devxive.com/license)
  *
  * @since		5.0
+ *
+ * NOTE: Normally installer will look for setup.php in component folder during component install. Usage of z_setup.php will prevent the execution during component installation,
+ *       remains directory consistency and will be included in the setup.php in the root folder of installation and executes in the postflight section of the install script.
  */
 
 // Set variables
@@ -20,8 +23,11 @@ $message = array();
 $element = 'com_xivetranscorder';
 $parent_menu = 'com_xiveirm';
 
+// Set the include path for the table class
+JTable::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_xivetranscorder/tables');
+
 if ($package['installer']->getInstallType() == 'install') {
-	// Runs on update
+	// Runs on install
 
 	// Restore assets from backup
 	if (NFWInstallerHelper::restoreAssets($element)) {
@@ -39,7 +45,7 @@ if ($package['installer']->getInstallType() == 'install') {
 	/*
 	 * Setup the parent category for all categories in the TOCA app
 	 */
-	$parentCategoryId = NFWTableCategory::store(array('extension' => 'com_xiveirm','title' => 'COM_XIVETRANSCORDER_PARENT_CATEGORY', 'alias' => 'com-xivetranscorder', 'access' => 2));
+	$parentCategoryId = NFWTableCategory::store(array('extension' => 'com_xiveirm','title' => 'COM_XIVEIRM_CATEGORY_PARENT_XIVETRANSCORDER', 'alias' => 'com-xivetranscorder', 'access' => 2, 'language' => '*'));
 	if ($parentCategoryId) {
 		$message[] = '<i class="icon-ok"></i> XiveTransCorder category ... OK';
 	} else {
@@ -48,41 +54,40 @@ if ($package['installer']->getInstallType() == 'install') {
 
 
 	/*
-	 * Set transport main category
+	 * Set parent transportations category
 	 */
-	$parentTransportCategoryId = NFWTableCategory::store(array('extension' => 'com_xiveirm','title' => 'COM_XIVETRANSCORDER_CATEGORY_PATIENT_TRANSPORT', 'alias' => 'toca-patient-transport', 'access' => 2, 'parent_id' => $parentCategoryId));
-	if ($parentTransportCategoryId) {
+	$parentTransportationsCategoryId = NFWTableCategory::store(array('extension' => 'com_xiveirm','title' => 'COM_XIVEIRM_CATEGORY_XIVETRANSCORDER_PARENT_TRANSPORTATIONS', 'alias' => 'xivetranscorder-transportations', 'access' => 2, 'language' => '*', 'parent_id' => $parentCategoryId));
+	if ($parentTransportationsCategoryId) {
+		$message[] = '<i class="icon-ok"></i> Set parent transportations category ... OK';
+
 		//Set transport related categories
-		$tc1 = NFWTableCategory::store(array('extension' => 'com_xiveirm','title' => 'COM_XIVETRANSCORDER_CATEGORY_PATIENT_TRANSPORT', 'alias' => 'toca-patient-transport', 'access' => 2, 'parent_id' => $parentTransportCategoryId));
-		$tc2 = NFWTableCategory::store(array('extension' => 'com_xiveirm','title' => 'COM_XIVETRANSCORDER_CATEGORY_QUALIFIED_PATIENT_AMBULANCE_TRANSPORT', 'alias' => 'toca-qualified-patient-transport', 'access' => 2, 'parent_id' => $parentTransportCategoryId));
-		$tc3 = NFWTableCategory::store(array('extension' => 'com_xiveirm','title' => 'COM_XIVETRANSCORDER_CATEGORY_STUFF_TRANSPORT', 'alias' => 'toca-stuff-transport', 'access' => 2, 'parent_id' => $parentTransportCategoryId));
-		$tc4 = NFWTableCategory::store(array('extension' => 'com_xiveirm','title' => 'COM_XIVETRANSCORDER_CATEGORY_DESPATCHING_ORDER', 'alias' => 'toca-despatcher-transport', 'access' => 2, 'parent_id' => $parentTransportCategoryId));
-		$tc5 = NFWTableCategory::store(array('extension' => 'com_xiveirm','title' => 'COM_XIVETRANSCORDER_CATEGORY_TRANSPORT_TEMPLATE', 'alias' => 'toca-transport-template', 'access' => 2, 'parent_id' => $parentTransportCategoryId));
+		$tc1 = NFWTableCategory::store(array('extension' => 'com_xiveirm','title' => 'COM_XIVEIRM_CATEGORY_XIVETRANSCORDER_TRANSPORTATIONS_PATIENT_TRANSPORT', 'alias' => 'transportations-patient-transport', 'access' => 2, 'language' => '*', 'parent_id' => $parentTransportationsCategoryId));
+		$tc2 = NFWTableCategory::store(array('extension' => 'com_xiveirm','title' => 'COM_XIVEIRM_CATEGORY_XIVETRANSCORDER_TRANSPORTATIONS_QUALIFIED_PATIENT_AMBULANCE_TRANSPORT', 'alias' => 'transportations-qualified-patient-transport', 'access' => 2, 'language' => '*', 'parent_id' => $parentTransportationsCategoryId));
+		$tc3 = NFWTableCategory::store(array('extension' => 'com_xiveirm','title' => 'COM_XIVEIRM_CATEGORY_XIVETRANSCORDER_TRANSPORTATIONS_STUFF_TRANSPORT', 'alias' => 'transportations-stuff-transport', 'access' => 2, 'language' => '*', 'parent_id' => $parentTransportationsCategoryId));
+		$tc4 = NFWTableCategory::store(array('extension' => 'com_xiveirm','title' => 'COM_XIVEIRM_CATEGORY_XIVETRANSCORDER_TRANSPORTATIONS_DESPATCHING_ORDER', 'alias' => 'transportations-despatcher-transport', 'access' => 2, 'language' => '*', 'parent_id' => $parentTransportationsCategoryId));
+		$tc5 = NFWTableCategory::store(array('extension' => 'com_xiveirm','title' => 'COM_XIVEIRM_CATEGORY_XIVETRANSCORDER_TRANSPORTATIONS_TRANSPORT_TEMPLATE', 'alias' => 'transportations-transport-template', 'access' => 2, 'language' => '*', 'parent_id' => $parentTransportationsCategoryId));
 		if ($tc1 && $tc2 && $tc3 && $tc4 && $tc5 ) {
-			$message[] = '<i class="icon-ok"></i> Set transport related categories ... OK';
+			$message[] = '<i class="icon-ok"></i> Set transportations related categories ... OK';
 		} else {
 			$message[] = '<i class="icon-cancel"></i> Set transport related categories ... FAILED!';
 		}
-
-		$message[] = '<i class="icon-ok"></i> Set transport main category ... OK';
 	} else {
-		$message[] = '<i class="icon-cancel"></i> Set transport main category ... FAILED';
+		$message[] = '<i class="icon-cancel"></i> Set parent transportations category ... FAILED';
 	}
 
 
 	/*
-	 * Set main transport form category
+	 * Set parent transport form category
 	 */
-	$parentFormCategoryId = NFWTableCategory::store(array('extension' => 'com_xiveirm','title' => 'COM_XIVETRANSCORDER_CATEGORY_PATIENT_TRANSPORT', 'alias' => 'toca-patient-transport', 'access' => 2, 'parent_id' => $parentCategoryId));
+	$parentFormCategoryId = NFWTableCategory::store(array('extension' => 'com_xiveirm','title' => 'COM_XIVEIRM_CATEGORY_XIVETRANSCORDER_PARENT_TRANSPORT_FORM', 'alias' => 'xivetranscorder-transport-form', 'access' => 2, 'language' => '*', 'parent_id' => $parentCategoryId));
 	if ($parentFormCategoryId) {
-		$message[] = '<i class="icon-ok"></i> Set main transport form category ... OK';
-	} else {
-		$message[] = '<i class="icon-cancel"></i> Set main transport form category ... FAILED';
-	}
+		$message[] = '<i class="icon-ok"></i> Set parent transport form category ... OK';
 
 		//Set transport device category
-		$transportDeviceId = NFWTableCategory::store(array('extension' => 'com_xiveirm', 'title' => 'COM_XIVETRANSCORDER_CATEGORY_TRANSPORT_DEVICE', 'alias' => 'transport-devices', 'access' => 2, 'parent_id' => $parentFormCategoryId));
+		$transportDeviceId = NFWTableCategory::store(array('extension' => 'com_xiveirm', 'title' => 'COM_XIVEIRM_CATEGORY_XIVETRANSCORDER_TRANSPORT_FORM_TRANSPORT_DEVICE', 'alias' => 'transport-form-transport-devices', 'access' => 2, 'language' => '*', 'parent_id' => $parentFormCategoryId));
 		if($transportDeviceId) {
+			$message[] = '<i class="icon-ok"></i> Set transport device category ... OK';
+
 			//Set transport device values
 			$rows[] = array('client_id' => 2, 'catid' => $transportDeviceId, 'opt_value' => 'TTV', 'opt_name' => 'COM_XIVETRANSCORDER_OPTION_TRANSPORT_DEVICE_TAXI_TRANSPORT_VEHICLE', 'access' => 2);
 			$rows[] = array('client_id' => 2, 'catid' => $transportDeviceId, 'opt_value' => 'RTV', 'opt_name' => 'COM_XIVETRANSCORDER_OPTION_TRANSPORT_DEVICE_RENTAL_TRANSPORT_VEHICLE', 'access' => 2);
@@ -99,15 +104,15 @@ if ($package['installer']->getInstallType() == 'install') {
 			} else {
 				$message[] = '<i class="icon-cancel"></i> Form - Set transport device values ... FAILED';
 			}
-
-			$message[] = '<i class="icon-ok"></i> Set transport device category ... OK';
 		} else {
 			$message[] = '<i class="icon-cancel"></i> Set transport device category ... FAILED';
 		}	
 
 		//Set transport type category
-		$transportTypeId = NFWTableCategory::store(array('extension' => 'com_xiveirm', 'title' => 'COM_XIVETRANSCORDER_CATEGORY_TRANSPORT_TYPE', 'alias' => 'transport-types', 'access' => 2, 'parent_id' => $parentFormCategoryId));
+		$transportTypeId = NFWTableCategory::store(array('extension' => 'com_xiveirm', 'title' => 'COM_XIVEIRM_CATEGORY_XIVETRANSCORDER_TRANSPORT_FORM_TRANSPORT_TYPE', 'alias' => 'transport-form-transport-types', 'access' => 2, 'language' => '*', 'parent_id' => $parentFormCategoryId));
 		if($transportTypeId) {
+			$message[] = '<i class="icon-ok"></i> Set transport type category ... OK';
+
 			//Set transport type values
 			$rows[] = array('client_id' => 2, 'catid' => $transportTypeId, 'opt_value' => 'ATW', 'opt_name' => 'COM_XIVETRANSCORDER_OPTION_TRANSPORT_TYPE_ABLE_TO_WALK', 'access' => 2);
 			$rows[] = array('client_id' => 2, 'catid' => $transportTypeId, 'opt_value' => 'WC', 'opt_name' => 'COM_XIVETRANSCORDER_OPTION_TRANSPORT_TYPE_WHEELCHAIR', 'access' => 2);
@@ -124,15 +129,15 @@ if ($package['installer']->getInstallType() == 'install') {
 			} else {
 				$message[] = '<i class="icon-cancel"></i> Form - Set transport type values ... FAILED';
 			}
-
-			$message[] = '<i class="icon-ok"></i> Set transport type category ... OK';
 		} else {
 			$message[] = '<i class="icon-cancel"></i> Set transport device category ... FAILED';
 		}	
 
 		//Set order type category
-		$orderTypeId = NFWTableCategory::store(array('extension' => 'com_xiveirm', 'title' => 'COM_XIVETRANSCORDER_CATEGORY_ORDER_TYPE', 'alias' => 'order-types', 'access' => 2, 'parent_id' => $parentFormCategoryId));
+		$orderTypeId = NFWTableCategory::store(array('extension' => 'com_xiveirm', 'title' => 'COM_XIVEIRM_CATEGORY_XIVETRANSCORDER_TRANSPORT_FORM_ORDER_TYPE', 'alias' => 'transport-form-order-types', 'access' => 2, 'language' => '*', 'parent_id' => $parentFormCategoryId));
 		if($transportDeviceId) {
+			$message[] = '<i class="icon-ok"></i> Set order type category ... OK';
+
 			//Set order type values
 			$rows[] = array('client_id' => 2, 'catid' => $orderTypeId, 'opt_value' => 'AMBU', 'opt_name' => 'COM_XIVETRANSCORDER_OPTION_ORDER_TYPE_AMBULANT', 'access' => 2);
 			$rows[] = array('client_id' => 2, 'catid' => $orderTypeId, 'opt_value' => 'ADM', 'opt_name' => 'COM_XIVETRANSCORDER_OPTION_ORDER_TYPE_ADMISSION', 'access' => 2);
@@ -157,13 +162,14 @@ if ($package['installer']->getInstallType() == 'install') {
 			if($return) {
 				$message[] = '<i class="icon-ok"></i> Form - Set order type values ... OK';
 			} else {
-				$message[] = '<i class="icon-ok"></i> Form - Set order type values ... FAILED';
+				$message[] = '<i class="icon-cancel"></i> Form - Set order type values ... FAILED';
 			}
-
-			$message[] = '<i class="icon-ok"></i> Set order type category ... OK';
 		} else {
 			$message[] = '<i class="icon-cancel"></i> Set order type category ... FAILED';
 		}	
+	} else {
+		$message[] = '<i class="icon-cancel"></i> Set parent transport form category ... FAILED';
+	}
 } else {
 	// Runs on update
 }
