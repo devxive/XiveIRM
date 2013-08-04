1da9ad3f1a70502bb5ec47778866a43ce2849a1d
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-08-04 18:19:16 +0200

    added new plugin helper class

---
+++ source/libraries/xiveirm/plugin/helper.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @project		XAP Project - Xive-Application-Platform
+ * @subProject	XiveIRM - Interoperable Relationship Management System
+ *
+ * @package		XiveIRM
+ * @subPackage	Library
+ * @version		6.0
+ *
+ * @author		devXive - research and development <support@devxive.com> (http://www.devxive.com)
+ * @copyright		Copyright (C) 1997 - 2013 devXive - research and development. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @assetsLicense	devXive Proprietary Use License (http://www.devxive.com/license)
+ *
+ * @since		3.2
+ */
+
+defined('_NFW_FRAMEWORK') or die();
+
+/**
+ * Component helper class
+ */
+abstract class IRMPluginHelper
+{
+	/*
+	 * Method to get a list of avialable plugins for the related app
+	 *
+	 * @return    object    
+	 */
+	public function getPlugins()
+	{
+	}
+}
