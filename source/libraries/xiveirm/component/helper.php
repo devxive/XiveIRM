4dcefc24edd765ffe7f572ac166817775f5e1ef0
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-08-08 02:39:56 +0200

    changed get_global_client id settings

--- source/libraries/xiveirm/component/helper.php
+++ source/libraries/xiveirm/component/helper.php
@@ -29,8 +29,23 @@ abstract class IRMComponentHelper
 	 */
 	public function getGlobalClientId()
 	{
-		if($global_client_id = JComponentHelper::getParams('com_xiveirm')->get('global_group')) {
+		if( $global_client_id = JComponentHelper::getParams('com_xiveirm')->get('global_group') ) {
 			return $global_client_id;
+		} else {
+			return false;
+		}
+	}
+
+
+	/*
+	 * Method to get the global viewlevel / accesslevel as set in the component settings
+	 *
+	 * @return		int	Return the global client_id as set in the component settings, else false.
+	 */
+	public function getGlobalViewLvl()
+	{
+		if( $global_viewlevel = JComponentHelper::getParams('com_xiveirm')->get('access') ) {
+			return $global_viewlevel;
 		} else {
 			return false;
 		}
