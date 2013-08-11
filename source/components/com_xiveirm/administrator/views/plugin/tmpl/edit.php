8dc5ada3e67e619091fed179b75e850b16a2d6e2
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-08-11 20:31:28 +0200

    added plugins

---
+++ source/components/com_xiveirm/administrator/views/plugin/tmpl/edit.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * @version     1.0.0
+ * @package     com_xiveirm
+ * @copyright   Copyright (C) 2013. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @author      Lahmizzar <lahmizzar@devxive.com> - http://walz.ag
+ */
+// no direct access
+defined('_JEXEC') or die;
+
+JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
+JHtml::_('behavior.tooltip');
+JHtml::_('behavior.formvalidation');
+JHtml::_('formbehavior.chosen', 'select');
+JHtml::_('behavior.keepalive');
+
+// Import CSS
+$document = JFactory::getDocument();
+$document->addStyleSheet('components/com_xiveirm/assets/css/xiveirm.css');
+?>
+<script type="text/javascript">
+    js = jQuery.noConflict();
+    js(document).ready(function(){
+        
+    });
+    
+    Joomla.submitbutton = function(task)
+    {
+        if(task == 'plugin.cancel'){
+            Joomla.submitform(task, document.getElementById('plugin-form'));
+        }
+        else{
+            
+            if (task != 'plugin.cancel' && document.formvalidator.isValid(document.id('plugin-form'))) {
+                
+                Joomla.submitform(task, document.getElementById('plugin-form'));
+            }
+            else {
+                alert('<?php echo $this->escape(JText::_('JGLOBAL_VALIDATION_FORM_FAILED')); ?>');
+            }
+        }
+    }
+</script>
+
+<form action="<?php echo JRoute::_('index.php?option=com_xiveirm&layout=edit&id=' . (int) $this->item->id); ?>" method="post" enctype="multipart/form-data" name="adminForm" id="plugin-form" class="form-validate">
+    <div class="row-fluid">
+        <div class="span10 form-horizontal">
+            <fieldset class="adminform">
+
+                			<div class="control-group">
+				<div class="control-label"><?php echo $this->form->getLabel('id'); ?></div>
+				<div class="controls"><?php echo $this->form->getInput('id'); ?></div>
+			</div>
+			<div class="control-group">
+				<div class="control-label"><?php echo $this->form->getLabel('client_id'); ?></div>
+				<div class="controls"><?php echo $this->form->getInput('client_id'); ?></div>
+			</div>
+			<div class="control-group">
+				<div class="control-label"><?php echo $this->form->getLabel('plugin'); ?></div>
+				<div class="controls"><?php echo $this->form->getInput('plugin'); ?></div>
+			</div>
+			<div class="control-group">
+				<div class="control-label"><?php echo $this->form->getLabel('catid'); ?></div>
+				<div class="controls"><?php echo $this->form->getInput('catid'); ?></div>
+			</div>
+			<div class="control-group">
+				<div class="control-label"><?php echo $this->form->getLabel('config'); ?></div>
+				<div class="controls"><?php echo $this->form->getInput('config'); ?></div>
+			</div>
+			<div class="control-group">
+				<div class="control-label"><?php echo $this->form->getLabel('state'); ?></div>
+				<div class="controls"><?php echo $this->form->getInput('state'); ?></div>
+			</div>
+
+
+            </fieldset>
+        </div>
+
+        <div class="clr"></div>
+
+<?php if (JFactory::getUser()->authorise('core.admin','xiveirm')): ?>
+	<div class="fltlft" style="width:86%;">
+		<fieldset class="panelform">
+			<?php echo JHtml::_('sliders.start', 'permissions-sliders-'.$this->item->id, array('useCookie'=>1)); ?>
+			<?php echo JHtml::_('sliders.panel', JText::_('ACL Configuration'), 'access-rules'); ?>
+			<?php echo $this->form->getInput('rules'); ?>
+			<?php echo JHtml::_('sliders.end'); ?>
+		</fieldset>
+	</div>
+<?php endif; ?>
+
+        <input type="hidden" name="task" value="" />
+        <?php echo JHtml::_('form.token'); ?>
+
+    </div>
+</form>
---
+++ source/components/com_xiveirm/administrator/views/plugins/view.html.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * @version     1.0.0
+ * @package     com_xiveirm
+ * @copyright   Copyright (C) 2013. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @author      Lahmizzar <lahmizzar@devxive.com> - http://walz.ag
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.view');
+
+/**
+ * View class for a list of Xiveirm.
+ */
+class XiveirmViewPlugins extends JViewLegacy
+{
+	protected $items;
+	protected $pagination;
+	protected $state;
+
+	/**
+	 * Display the view
+	 */
+	public function display($tpl = null)
+	{
+		$this->state		= $this->get('State');
+		$this->items		= $this->get('Items');
+		$this->pagination	= $this->get('Pagination');
+
+		// Check for errors.
+		if (count($errors = $this->get('Errors'))) {
+			throw new Exception(implode("\n", $errors));
+		}
+        
+		XiveirmHelper::addSubmenu('plugins');
+        
+		$this->addToolbar();
+        
+        $this->sidebar = JHtmlSidebar::render();
+		parent::display($tpl);
+	}
+
+	/**
+	 * Add the page title and toolbar.
+	 *
+	 * @since	1.6
+	 */
+	protected function addToolbar()
+	{
+		require_once JPATH_COMPONENT.'/helpers/xiveirm.php';
+
+		$state	= $this->get('State');
+		$canDo	= XiveirmHelper::getActions($state->get('filter.category_id'));
+
+		JToolBarHelper::title(JText::_('COM_XIVEIRM_TITLE_PLUGINS'), 'plugins.png');
+
+        //Check if the form exists before showing the add/edit buttons
+        $formPath = JPATH_COMPONENT_ADMINISTRATOR.'/views/plugin';
+        if (file_exists($formPath)) {
+
+            if ($canDo->get('core.create')) {
+			    JToolBarHelper::addNew('plugin.add','JTOOLBAR_NEW');
+		    }
+
+		    if ($canDo->get('core.edit') && isset($this->items[0])) {
+			    JToolBarHelper::editList('plugin.edit','JTOOLBAR_EDIT');
+		    }
+
+        }
+
+		if ($canDo->get('core.edit.state')) {
+
+            if (isset($this->items[0]->state)) {
+			    JToolBarHelper::divider();
+			    JToolBarHelper::custom('plugins.publish', 'publish.png', 'publish_f2.png','JTOOLBAR_PUBLISH', true);
+			    JToolBarHelper::custom('plugins.unpublish', 'unpublish.png', 'unpublish_f2.png', 'JTOOLBAR_UNPUBLISH', true);
+            } else if (isset($this->items[0])) {
+                //If this component does not use state then show a direct delete button as we can not trash
+                JToolBarHelper::deleteList('', 'plugins.delete','JTOOLBAR_DELETE');
+            }
+
+            if (isset($this->items[0]->state)) {
+			    JToolBarHelper::divider();
+			    JToolBarHelper::archiveList('plugins.archive','JTOOLBAR_ARCHIVE');
+            }
+            if (isset($this->items[0]->checked_out)) {
+            	JToolBarHelper::custom('plugins.checkin', 'checkin.png', 'checkin_f2.png', 'JTOOLBAR_CHECKIN', true);
+            }
+		}
+        
+        //Show trash and delete for components that uses the state field
+        if (isset($this->items[0]->state)) {
+		    if ($state->get('filter.state') == -2 && $canDo->get('core.delete')) {
+			    JToolBarHelper::deleteList('', 'plugins.delete','JTOOLBAR_EMPTY_TRASH');
+			    JToolBarHelper::divider();
+		    } else if ($canDo->get('core.edit.state')) {
+			    JToolBarHelper::trash('plugins.trash','JTOOLBAR_TRASH');
+			    JToolBarHelper::divider();
+		    }
+        }
+
+		if ($canDo->get('core.admin')) {
+			JToolBarHelper::preferences('com_xiveirm');
+		}
+        
+        //Set sidebar action - New in 3.0
+		JHtmlSidebar::setAction('index.php?option=com_xiveirm&view=plugins');
+        
+        $this->extra_sidebar = '';
+        
+		//Filter for the field client_id
+		$this->extra_sidebar .= '<small><label for="filter_client_id">Client Id</label></small>';
+		$this->extra_sidebar .= JHtmlList::users('filter_client_id', $this->state->get('filter.client_id'), 1, 'onchange="this.form.submit();"');
+		JHtmlSidebar::addFilter(
+			JText::_("JOPTION_SELECT_ACCESS"),
+			'filter_plugin',
+			JHtml::_('select.options', JHtml::_("access.assetgroups", true, true), "value", "text", $this->state->get('filter.plugin'), true)
+
+		);
+
+		JHtmlSidebar::addFilter(
+			JText::_("JOPTION_SELECT_CATEGORY"),
+			'filter_catid',
+			JHtml::_('select.options', JHtml::_('category.options', 'com_xiveirm.plugins.catid'), "value", "text", $this->state->get('filter.catid'))
+
+		);
+
+		JHtmlSidebar::addFilter(
+
+			JText::_('JOPTION_SELECT_PUBLISHED'),
+
+			'filter_published',
+
+			JHtml::_('select.options', JHtml::_('jgrid.publishedOptions'), "value", "text", $this->state->get('filter.state'), true)
+
+		);
+
+        
+	}
+    
+	protected function getSortFields()
+	{
+		return array(
+		'a.id' => JText::_('JGRID_HEADING_ID'),
+		'a.client_id' => JText::_('COM_XIVEIRM_PLUGINS_CLIENT_ID'),
+		'a.plugin' => JText::_('COM_XIVEIRM_PLUGINS_PLUGIN'),
+		'a.catid' => JText::_('COM_XIVEIRM_PLUGINS_CATID'),
+		'a.state' => JText::_('JSTATUS'),
+		'a.ordering' => JText::_('JGRID_HEADING_ORDERING'),
+		);
+	}
+
+    
+}
