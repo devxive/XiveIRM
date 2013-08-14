629e34f67e9d138290d4a55d06fade913553661a
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-08-14 23:13:53 +0200

    added getContactObject method

--- source/libraries/xiveirm/item/helper.php
+++ source/libraries/xiveirm/item/helper.php
@@ -95,4 +95,82 @@ class IRMItemHelper
 			return false;
 		}
 	}
+
+
+	/*
+	 * Method to get the contact and all related tabApps in a single object. Mostly used for other extensions, such as the XiveTransCorder App.
+	 *
+	 * @since 5.0
+	 */
+	public function getContactObject($contactId)
+	{
+		$results = new JObject();
+		$db = JFactory::getDbo();
+
+		// Create a new query object for the contact.
+		$query = $db->getQuery(true);
+
+		// Prepare the query
+		$query
+			->select('a.*')
+			->from('#__xiveirm_contacts AS a')
+			->where('a.id = ' . $contactId . '');
+
+		// Join over the users for the checked out user.
+		$query->select('uc.name AS editor_name');
+		$query->join('LEFT', '#__users AS uc ON uc.id=a.checked_out');
+
+		// Join over the created by field 'created_by'
+		$query->select('created_by.name AS creator_name');
+		$query->join('LEFT', '#__users AS created_by ON created_by.id = a.created_by');
+
+		// Join over the modified by field 'modified_by'
+		$query->select('modified_by.name AS modified_name');
+		$query->join('LEFT', '#__users AS modified_by ON modified_by.id = a.modified_by');
+
+		// Join over the category 'catid'
+		$query->select('catid.title AS catid_title');
+		$query->join('LEFT', '#__categories AS catid ON catid.id = a.catid');
+
+		// Join over the category 'gender'
+		$query->select('gender.title AS gender_title');
+		$query->join('LEFT', '#__categories AS gender ON gender.id = a.gender');
+
+		// Join over the flags
+		$query->select('flags.flag AS flagged');
+		$query->join('LEFT', '#__xiveirm_flags AS flags ON flags.item = CONCAT(\'contacts.\', a.id)');
+
+		// Join over the tabapps
+//		$query->select(array('tabapps.app_key as app_key', 'tabapps.app_value AS app_value'));
+//		$query->join('LEFT', '#__xiveirm_contacts_appvalues AS tabapps ON tabapps.contacts_id = a.id');
+
+		$db->setQuery($query);
+		$coreResults = $db->loadObjectList();
+
+		foreach($coreResults as $coreResult) {
+			$results->contact = $coreResult;
+		}
+
+		// Create a new query object for the tabApps.
+		$query = $db->getQuery(true);
+
+		// Prepare the query
+		$query
+			->select(array('b.app_key as app_key', 'b.app_value AS app_value'))
+			->from('#__xiveirm_contacts_appvalues AS b')
+			->where('b.contacts_id = ' . $contactId . '');
+
+		$db->setQuery($query);
+		$tabResults = $db->loadObjectList();
+
+		$tabsObject = new stdClass();
+		foreach($tabResults as $tabResult) {
+			$appKey = $tabResult->app_key;
+			$tabsObject->$appKey = json_decode($tabResult->app_value);
+		}
+
+		$results->tabs = $tabsObject;
+
+		return $results;
+	}
 }
