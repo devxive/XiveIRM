8411d9c61e0f957e32a2a189f436ae7fcb632d5c
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-08-18 21:22:30 +0200

    changed clauses to format the name for list options

--- source/libraries/xiveirm/form/name.php
+++ source/libraries/xiveirm/form/name.php
@@ -18,7 +18,7 @@
 defined('_NFW_FRAMEWORK') or die();
 
 /**
- * Component helper class
+ * Form or table helper class to format names
  */
 class IRMFormName
 {
@@ -33,46 +33,74 @@ class IRMFormName
 	public static function formatPoiName( $values, $checkmark = true )
 	{
 		// Preformat in the IRMHtmlBuilder class similar to client id method
-		$id                  = isset($values->id)                  ? $values->id                  : false;
-		$address_name        = isset($values->address_name)        ? $values->address_name        : false;
-		$address_name_add    = isset($values->address_name_add)    ? $values->address_name_add    : false;
-		$company             = isset($values->company)             ? $values->company             : false;
-		$last_name           = isset($values->last_name)           ? $values->last_name           : false;
-		$first_name          = isset($values->first_name)          ? $values->first_name          : false;
-		$system_checked      = isset($values->system_checked)      ? $values->system_checked      : false;
-		$client_checked      = isset($values->client_checked)      ? $values->client_checked      : false;
+		$id                  = isset($values->id) || isset($values->contact_id)    ? $values->id                  : false;
+		$address_name        = isset($values->address_name)                        ? $values->address_name        : false;
+		$address_name_add    = isset($values->address_name_add)                    ? $values->address_name_add    : false;
+		$company             = isset($values->company)                             ? $values->company             : false;
+		$last_name           = isset($values->last_name)                           ? $values->last_name           : false;
+		$first_name          = isset($values->first_name)                          ? $values->first_name          : false;
+		$system_checked      = isset($values->system_checked)                      ? $values->system_checked      : false;
+		$client_checked      = isset($values->client_checked)                      ? $values->client_checked      : false;
 
 		$opt_name = '';
 
-		// Build the placeholder between
-		if ( $system_checked ) {
-			$opt_name .= '<i class="icon-ok-sign"></i> ';
-		} else if ( $client_checked ) {
-			$opt_name .= '* ';
-		}
-
-		if ( $address_name && $address_name_add ) {
-			$opt_name .= $address_name . ' (' . $address_name_add . ')';
-		} else if ( $address_name ) {
-			$opt_name .= $address_name;
-		} else if ( !$address_name && !$address_name_add && $company ) {
+		// If only company or company and name
+		if ( $company && (!$last_name || !$first_name) ) {
 			$opt_name .= $company;
-		} else if ( !$address_name && $address_name_add && $company ) {
-			$opt_name .= $company . ' (' . $address_name_add . ')';
-		} else if ( !$address_name && !$address_name_add && $company && $last_name && $first_name ) {
-			$opt_name .= $company . ' - ' . $last_name . ', ' . $first_name;
-		} else if ( !$address_name && !$address_name_add && !$company && $last_name && $first_name ) {
-			$opt_name .= $last_name . ', ' . $first_name;
-		} else if ( !$address_name && !$address_name_add && !$company && $last_name ) {
-			$opt_name .= $last_name;
-		} else if ( !$address_name && !$address_name_add && !$company && $first_name ) {
-			$opt_name .= $first_name;
+			$check_before = true;
+		} else if ( $company && ($last_name || $first_name) ) {
+			$opt_name .= '<small>' . $company . '</small><br>';
+			if ( $last_name ) {
+				$opt_name .= $last_name . ', (****)';
+			} else {
+				$opt_name .= '(****), ' . $first_name;
+			}
+			$check_before = true;
+		} else if ( ($last_name || $first_name) && !$company ) {
+			if ( $last_name && $first_name ) {
+				$opt_name .= $last_name . ', ' . $first_name;
+			} else if ( $last_name ) {
+				$opt_name .= $last_name . ', (****)';
+			} else {
+				$opt_name .= '(****), ' . $first_name;
+			}
+			$check_before = true;
 		} else {
-			$opt_name = false;
+			$opt_name .= '';
+			$check_before = false;
 		}
 
-		if ( !$opt_name ) {
+		// If block 1 and block 2, add new line
+		if ( $check_before && ($address_name || $address_name_add) ) {
+			$opt_name .= '<br>';
+		}
+
+		// If only address_name or address_name and address_name_add
+		if ( $address_name && !$address_name_add ) {
+			$opt_name .= $address_name;
+			$check_after = true;
+		} else if ( $address_name && $address_name_add ) {
+			$opt_name .= $address_name . ' ( ' . $address_name_add . ' )';
+			$check_after = true;
+		} else if ( !$address_name && $address_name_add ) {
+			$opt_name .= $address_name_add;
+			$check_after = true;
+		} else {
+			$opt_name .= '';
+			$check_after = false;
+		}
+
+		if ( !$check_before && !$check_after ) {
 			$opt_name = '*** Undefined name for ID: ' . $id . ' ***';
+		}
+
+		// Adding checkmark icon/sign
+		if ( $checkmark ) {
+			if ( $system_checked ) {
+				$opt_name .= ' <i class="icon-ok-sign"></i>';
+			} else if ( $client_checked ) {
+				$opt_name .= ' *';
+			}
 		}
 
 		return $opt_name;
@@ -113,6 +141,91 @@ class IRMFormName
 
 		if ( !$opt_name ) {
 			$opt_name = '*** Undefined name for ID: ' . $id . ' ***';
+		}
+
+		return $opt_name;
+	}
+
+
+	/*
+	 * Method to format names
+	 *
+	 * @param     array      $values       The name of the component as stored in components folder (com_mycomponent)
+	 * @param     boolean    $checkmark    Show (true) or hide (false) checkmarks in the name string
+	 *
+	 * @return    string
+	 */
+	public static function formatListName( $values, $checkmark = true )
+	{
+		// Preformat in the IRMHtmlBuilder class similar to client id method
+		$id                  = isset($values->id) || isset($values->contact_id)    ? $values->id                  : false;
+		$address_name        = isset($values->address_name)                        ? $values->address_name        : false;
+		$address_name_add    = isset($values->address_name_add)                    ? $values->address_name_add    : false;
+		$company             = isset($values->company)                             ? $values->company             : false;
+		$last_name           = isset($values->last_name)                           ? $values->last_name           : false;
+		$first_name          = isset($values->first_name)                          ? $values->first_name          : false;
+		$system_checked      = isset($values->system_checked)                      ? $values->system_checked      : false;
+		$client_checked      = isset($values->client_checked)                      ? $values->client_checked      : false;
+
+		$opt_name = '';
+
+		// If only company or company and name
+		if ( $company && (!$last_name || !$first_name) ) {
+			$opt_name .= $company;
+			$check_before = true;
+		} else if ( $company && ($last_name || $first_name) ) {
+			$opt_name .= '<small>' . $company . '</small><br>';
+			if ( $last_name ) {
+				$opt_name .= $last_name . ', (****)';
+			} else {
+				$opt_name .= '(****), ' . $first_name;
+			}
+			$check_before = true;
+		} else if ( ($last_name || $first_name) && !$company ) {
+			if ( $last_name && $first_name ) {
+				$opt_name .= $last_name . ', ' . $first_name;
+			} else if ( $last_name ) {
+				$opt_name .= $last_name . ', (****)';
+			} else {
+				$opt_name .= '(****), ' . $first_name;
+			}
+			$check_before = true;
+		} else {
+			$opt_name .= '';
+			$check_before = false;
+		}
+
+		// If block 1 and block 2, add new line
+		if ( $check_before && ($address_name || $address_name_add) ) {
+			$opt_name .= '<br>';
+		}
+
+		// If only address_name or address_name and address_name_add
+		if ( $address_name && !$address_name_add ) {
+			$opt_name .= $address_name;
+			$check_after = true;
+		} else if ( $address_name && $address_name_add ) {
+			$opt_name .= $address_name . ' ( ' . $address_name_add . ' )';
+			$check_after = true;
+		} else if ( !$address_name && $address_name_add ) {
+			$opt_name .= $address_name_add;
+			$check_after = true;
+		} else {
+			$opt_name .= '';
+			$check_after = false;
+		}
+
+		if ( !$check_before && !$check_after ) {
+			$opt_name = '*** Undefined name for ID: ' . $id . ' ***';
+		}
+
+		// Adding checkmark icon/sign
+		if ( $checkmark ) {
+			if ( $system_checked ) {
+				$opt_name .= ' <i class="icon-ok-sign"></i>';
+			} else if ( $client_checked ) {
+				$opt_name .= ' *';
+			}
 		}
 
 		return $opt_name;
