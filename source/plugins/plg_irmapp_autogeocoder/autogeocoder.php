a1d3e8155ee8db71f5e5c96ab47c6671816b69a2
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-08-23 20:16:45 +0200

    added helper classes to fit various situations during input

--- source/plugins/plg_irmapp_autogeocoder/autogeocoder.php
+++ source/plugins/plg_irmapp_autogeocoder/autogeocoder.php
@@ -33,13 +33,13 @@ class PlgIrmAppAutogeocoder extends JPlugin
 	 */
 	public function __construct(& $subject, $config)
 	{
+		parent::__construct($subject, $config);
+
 		// TODO: Check first if we have an internet connection, else return false or throw message htat there is no connection and that this geocoder doesn't work
 		// NFWConnectionHelper::checkOnline(); or something else
 
 		$this->appKey = 'autogeocoder';
 		$this->loadLanguage();
-
-		parent::__construct($subject, $config);
 	}
 
 
@@ -52,19 +52,45 @@ class PlgIrmAppAutogeocoder extends JPlugin
 	 */
 	public function onBeforeContent( &$item = null, &$params = null )
 	{
-		// Init google maps and jquery gmap3 plugin
+		// Init google maps and jquery gmap3 plugin and helper js file
 		NFWGeoGmap3::initMap();
 		JHtml::_('script', 'plugins/irmapp/autogeocoder/assets/js/geo-helper.js', false, false, false, false);
 
 		NFWHtmlJavascript::detectChanges();
-		NFWHtmlJavascript::loadEasyPie('.ep-chart', false, false);
-
 
 		// Set the script
 		$script = "
-			function geocodeInputHelper() {
-				jQuery('#geocode-input-helper').fadeToggle('slow');
+			function callbackCodeAddress(results) {
+				if (results === 'ZERO_RESULTS') {
+					alertify.alert('<div class=\"modal-header\"><h3>Address verification failed!</h3></div><div class=\"modal-body\">Sorry, we can\'t verify the address you typed in! Please try the following:<ul><li>You have to type in a house number</li><li>Try to type in similar words for street or city</li><li>Try to type in the state or the country</li><li>Try to check the address on google maps</li></ul></div>');
+					return false;
+				}
+
+				var nameObserver = '.address-block[data-direction=\"' + results[0].transport.direction + '\"][data-order=\"' + results[0].transport.order + '\"]';
+
+				$(nameObserver + ' input[id*=\"address_street\"]').val(results[0].input_address.address_street);
+				$(nameObserver + ' input[id*=\"address_houseno\"]').val(results[0].input_address.address_houseno);
+				$(nameObserver + ' input[id*=\"address_zip\"]').val(results[0].input_address.address_zip);
+				$(nameObserver + ' input[id*=\"address_city\"]').val(results[0].input_address.address_city);
+				$(nameObserver + ' input[id*=\"address_region\"]').val(results[0].input_address.address_region);
+				$(nameObserver + ' input[id*=\"address_country\"]').val(results[0].input_address.address_country);
+
+				if( results[0].geometry.coords.lat && results[0].geometry.coords.lng && results[0].geometry.location_type === 'ROOFTOP') {
+					alertify.success('<i class=\"icon-globe\"></i> Valid address found!');
+					$('#b-address-geo-verified-1').addClass('green').removeClass('red');
+					$(nameObserver + ' input[id*=\"address_lat\"]').val(results[0].geometry.coords.lat);
+					$(nameObserver + ' input[id*=\"address_lng\"]').val(results[0].geometry.coords.lng);
+				} else {
+					alertify.warning = alertify.extend('warning');
+					alertify.warning('<i class=\"icon-globe red\"></i> There are only approximate geo coordinations for the given address. Pleasy try to type in more informations!');
+					$('#b-address-geo-verified-1').addClass('red').removeClass('green');
+					$(nameObserver + ' input[id*=\"address_lat\"]').val('');
+					$(nameObserver + ' input[id*=\"address_lng\"]').val('');
+				}
+
+				window.aaAutoGeocoder = results;
 			}
+
 
 			/*
 			 * Function to get the item cid (db id of the core item we are in. Should always ends --regex selector $-- with _cid)
@@ -84,51 +110,47 @@ class PlgIrmAppAutogeocoder extends JPlugin
 			 */
 			jQuery(document).ready(function() {
 				/*
-				 * Initals to build before any user event is triggered
-				 */
-				// Build and insert the geoIcon right after the #address-hash-verified icon
-				var geoIcon = '<span id=\"address-geo-verified\" class=\"small-margin-left\" style=\"vertical-align: middle;\"><i class=\"icon-globe\" style=\"font-size: 18px;\"></i></span>';
-				$(geoIcon).insertAfter('#address-hash-verified');
-
-				// Build and insert the auto geoInputHelper field before the #inner-address-block
-				var geoInputHelper = '<div id=\"address_auto_geocoder\" class=\"controls controls-row\">';
-					geoInputHelper += '<div class=\"alert\" style=\"padding: 8px !important; margin-bottom: 10px;\">';
-						geoInputHelper += '<input type=\"text\" class=\"input-control span12 red\" placeholder=\"Type in: Street HouseNo, City, State, Country\" onFocus=\"geocodeInputHelper()\" onBlur=\"geocodeInputHelper()\" style=\"margin: 0 !important; float: none;\"/>';
-						geoInputHelper += '<div id=\"geocode-input-helper\" class=\"center\" style=\"margin-top: 10px; display: none;\">';
-							geoInputHelper += '<small>';
-								geoInputHelper += 'Type in here the address the geocoder should find and validate. This can take up to 5 seconds!<br>';
-								geoInputHelper += '<em><strong>Please note that this field will not save its values!</strong></em>';
-							geoInputHelper += '</small>';
-						geoInputHelper += '</div>';
-					geoInputHelper += '</div>';
-				geoInputHelper += '</div>';
-				$(geoInputHelper).insertBefore('#inner-address-block');
-
-				// Append after #address-specific-options
-				var addressOptions = '<span id=\"geocode-progress\" class=\"icon-custom ep-chart xpopover pull-right\" data-original-title=\"Click here if you wish to check the address already filled out below\" data-percent=\"100\" data-size=\"22\" data-line-width=\"3\" data-animate=\"1500\" data-color=\"#EBA450\" style=\"top: 2px;\"></span>';
-				$('#address-specific-options').append(addressOptions);
-
-				/*
 				 * Checking section if we should or should not display any further options
 				 */
 				// If its an existing contact we have to hide the geocoder fields
 				if( getItemId() ) {
-					$('#geocode-progress, #address_auto_geocoder, .gverifier').hide();
+					$('#geocode-progress, #address_auto_geocoder').hide();
 
-					// Check for lat lng values and set the icon color
-					var iconLatLng = getAddress(null, 0);
+					// Check for lat lng values, set the icon color and init the map
+					var iconLatLng = getAddress('b', 1);
 					if( iconLatLng.address_lat && iconLatLng.address_lng ) {
-						$('#address-geo-verified').addClass('green').removeClass('red');
+						$('#b-address-geo-verified-1').show().addClass('green').removeClass('red');
+
+						// Initialize map with existing latlng (initLat, initLng, initZoom, initMarker)
+						$('#map-body').show();
+						initialize(iconLatLng.address_lat, iconLatLng.address_lng, 17, true);
 					} else {
-						$('#address-geo-verified').addClass('red').removeClass('green');
+						$('#b-address-geo-verified-1').show().addClass('red').removeClass('green');
+
+						// Initialize map on document ready function (initLat, initLng, initZoom, initMarker)
+						$('#map-body').show();
+						initialize(0, 0, 1, false);
 					}
+				} else {
+					$('#b-address-geo-verified-1').fadeIn(2500);
+					$('#b-address-geo-verified-1').show().addClass('red').removeClass('green');
+
+					// Initialize map on document ready function (initLat, initLng, initZoom, initMarker)
+					$('#map-body').show();
+					initialize(50, 9, 4, false);
+
+					// Show the geocoder field and the counter
+					$('.address_auto_geocoder, .easypie-progress').fadeIn('slow', function() {
+						$('.easypie-progress.ep-chart').data('easyPieChart').update(100);
+					});
+
 				}
 
 				// If user click edit button, we have to show the input field and graphical stuff
 				$('#loading-btn-edit').click(function() {
 					$(document).ajaxSuccess(function() {
-						$('#geocode-progress, #address_auto_geocoder').fadeIn('slow', function() {
-							$('#geocode-progress.ep-chart').data('easyPieChart').update(100);
+						$('.address_auto_geocoder, .easypie-progress').fadeIn('slow', function() {
+							$('.easypie-progress.ep-chart').data('easyPieChart').update(100);
 						});
 					});
 				});
@@ -136,26 +158,104 @@ class PlgIrmAppAutogeocoder extends JPlugin
 				// If user click update/save button, we have to hide the input field and graphical stuff
 				$('#loading-btn-save').click(function() {
 					$(document).ajaxSuccess(function() {
-						$('#geocode-progress, #address_auto_geocoder').hide();
+						$('.address_auto_geocoder, .easypie-progress').hide();
 					});
 				});
 
 
-				// Adding geoVerifier click funtion
-				$('.gverifier').click(function() {
+
+				// ##################### START OF TRIGGERED EVENTS TO THE GEOCODING PROCESS #########################
+				var timeoutId;
+
+				// Triggered on every change in the auto geocoder input field but only if its focused (this.value determines the actual field)
+				$('.address_auto_geocoder input').focus(function() {
 					// Get the direction and order position
 					var ownDirection = $(this).parents('.address-block').data('direction');
 					var ownOrder = $(this).parents('.address-block').data('order');
+					var nameObserver = '.address-block[data-direction=\"' + ownDirection + '\"][data-order=\"' + ownOrder + '\"]';
 
-					// Get the address values from appropriate dir and order, returned as string
-					var address = getAddress(ownDirection, ownOrder, true);
+					$(nameObserver + ' .geocode-input-helptext').slideToggle();
 
-					console.log('Direction: ' + ownDirection + ', Order: ' + ownOrder + ', Address: ' + address);
+					$(this).on('inputchange', function() {
+						window.clearTimeout(timeoutId);
+						var addressValue = $(nameObserver + ' .address_auto_geocoder input').val();
 
-					// Overgive the function the address values to do all the magic
-					geoFormatting(address);
+						// Trigger only if at least 5 digits typed in
+						if( addressValue.length > 5 ) {
+							timeoutId = window.timeoutId = setTimeout(function() {
+								codeAddress(addressValue, ownDirection, ownOrder);
+							}, 1500);
+
+							// If anything in this field has changed play with the animation
+							$('.easypie-progress.ep-chart').data('easyPieChart').update(100);
+							$('.easypie-progress.ep-chart').data('easyPieChart').update(0);
+						}
+					});
+				}).focusout(function() {
+					// Get the direction and order position
+					var ownDirection = $(this).parents('.address-block').data('direction');
+					var ownOrder = $(this).parents('.address-block').data('order');
+					var nameObserver = '.address-block[data-direction=\"' + ownDirection + '\"][data-order=\"' + ownOrder + '\"]';
+
+					$(nameObserver + ' .geocode-input-helptext').slideToggle();
 				});
 
+
+				// Triggered on every change in the inner-address-block (this.value determines the actual field)
+				$('.inner-address-block input').on('inputchange', function() {
+					window.clearTimeout(timeoutId);
+
+					// Get the direction and order position
+					var ownDirection = $(this).parents('.address-block').data('direction');
+					var ownOrder = $(this).parents('.address-block').data('order');
+					var nameObserver = '.address-block[data-direction=\"' + ownDirection + '\"][data-order=\"' + ownOrder + '\"]';
+
+					// try workaround to could change the housenumber. we have to remove the latlng if houseno is removed and add cecks below: if houseno is empty, remove latlng, in check below: if all set houseno >= 0 and latlng empty then trigger new
+					var checkHouseNo = $(nameObserver + ' input[id*=\"address_houseno\"]').val();
+					if( !checkHouseNo ) {
+						$(nameObserver + ' input[id*=\"address_lat\"]').val('');
+						$(nameObserver + ' input[id*=\"address_lng\"]').val('');
+					}
+
+					var latHelp = $(nameObserver + ' input[id*=\"address_lat\"]').val(),
+					lngHelp = $(nameObserver + ' input[id*=\"address_lng\"]').val();
+					if(  !latHelp && !lngHelp ) {
+						var latlngHelper = true;
+					}
+
+					// Prevent double trigger if above function is used
+					var addressValue = $(nameObserver + ' .address_auto_geocoder input').val();
+					if( addressValue.length > 0 ) {
+						return;
+					} else {
+						// Get the address values from appropriate dir and order, returned as string
+						var address = getAddress(ownDirection, ownOrder);
+
+						timeoutId = window.timeoutId = setTimeout(function() {
+							// Send address values to codeAddress function to get a coded address in the callbackCodeAddress function above that do all the magic
+							if(
+								(address.address_street !== '' && address.address_street.length >= 3 &&
+								address.address_houseno !== '' && address.address_houseno.length >= 1 &&
+								address.address_zip !== '' && address.address_zip.length >= 4 &&
+								address.address_city === '')
+							||
+								(address.address_street !== '' && address.address_street.length >= 3 &&
+								address.address_houseno !== '' && address.address_houseno.length >= 1 &&
+								address.address_zip === '' &&
+								address.address_city !== '' && address.address_city.length >= 2)
+							||
+								(address.address_street !== '' && address.address_street.length >= 3 &&
+								address.address_zip !== '' && address.address_zip.length >= 4 &&
+								address.address_city !== '' && address.address_city.length >= 2) && (address.address_houseno.length >= 0 && latlngHelper)
+							) {
+									codeAddress(address.string_name, ownDirection, ownOrder);
+							}
+						}, 1500);
+
+						$('.easypie-progress.ep-chart').data('easyPieChart').update(100);
+						$('.easypie-progress.ep-chart').data('easyPieChart').update(0);
+					}
+				});
 			});
 		";
 		JFactory::getDocument()->addScriptDeclaration($script);
@@ -216,84 +316,11 @@ class PlgIrmAppAutogeocoder extends JPlugin
 			$address_geo_verified = false;
 		}
 
-
-
-		$script = "
-			/*
-			 * Global ready function
-			 */
-			jQuery(document).ready(function() {
-
-
-			// ##################### have to add a timeout function to prevent realtime requests!
-
-				// Triggered on every change in the auto geocoder block (this.value determines the actual field)
-				$('#address_auto_geocoder input').on('inputchange', function() {
-					// Get and set the address vars to auto-geocoder and trigger onKeyUp
-					$('#location').val(this.value);
-					$('#location').trigger('auto-geocoder.onKeyUp');
-					
-					// If anything in this field has changed play with the animation
-					$('#geocode-progress.ep-chart').fadeIn('slow');
-					$('#geocode-progress.ep-chart').data('easyPieChart').update(100);
-					$('#geocode-progress.ep-chart').data('easyPieChart').update(0);
-				});
-
-
-				// Triggered on every change in the inner-address-block (this.value determines the actual field)
-				$('#inner-address-block input').on('inputchange', function() {
-					// Get the live var on every change
-					var address_name = ( $('#address_name').val() )     ? $('#address_name').val() + ' '     : '',
-					address_name_add = ( $('#address_name_add').val() ) ? $('#address_name_add').val() + ' ' : '',
-					address_street   = ( $('#address_street').val() )   ? $('#address_street').val() + ' '   : '',
-					address_houseno  = ( $('#address_houseno').val() )  ? $('#address_houseno').val() + ', ' : '',
-					address_zip      = ( $('#address_zip').val() )      ? $('#address_zip').val() + ' '      : '',
-					address_city     = ( $('#address_city').val() )     ? $('#address_city').val() + ', '    : '',
-					address_region   = ( $('#address_region').val() )   ? $('#address_region').val() + ' '   : '',
-					address_country  = ( $('#address_country').val() )  ? $('#address_country').val() + ' '  : '',
-					address_lat      = ( $('#address_lat').val() )      ? $('#address_lat').val()            : '',
-					address_lng      = ( $('#address_lng').val() )      ? $('#address_lng').val()            : '';
-
-					var address_full = address_name + address_name_add + address_street + address_houseno + address_zip + address_city + address_region + address_country;
-
-					$('#location').val(address_full);
-					$('#location').trigger('auto-geocoder.onKeyUp');
-
-					// If anything in this field has changed play with the animation
-					$('#geocode-progress.ep-chart').fadeIn('slow');
-					$('#geocode-progress.ep-chart').data('easyPieChart').update(100);
-					$('#geocode-progress.ep-chart').data('easyPieChart').update(0);
-				});
-
-
-				// Triggered on every change in lat lng vars
-				$('.address-block #geo-coords input').on('inputchange', function() {
-					// Check for values and set the icon color
-					var iconLatLng = getAddress(null, 0);
-					if( iconLatLng.address_lat && iconLatLng.address_lng ) {
-						$('#address-geo-verified').addClass('green').removeClass('red');
-					} else {
-						$('#address-geo-verified').addClass('red').removeClass('green');
-					}
-
-					// Get and set the address vars to auto-geocoder and trigger onKeyUp
-					$('#location').val(this.value);
-					$('#location').trigger('auto-geocoder.onKeyUp');
-				});
-			});
-		";
-		JFactory::getDocument()->addScriptDeclaration($script);
-
 		ob_start();
 		?>
 		<!---------- Begin output buffering: <?php echo $this->appKey; ?> ---------->
 
-		<pre>
-		<?php
-			$test='';
-			print_r($test);
-		?>
-		</pre>
+		<a onClick="clearMarker()">clicky</a>
 
 		<!---------- End output buffering: <?php echo $this->appKey; ?> ---------->
 		<?php
