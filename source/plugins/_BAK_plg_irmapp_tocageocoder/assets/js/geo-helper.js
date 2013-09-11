821899d11f4581a457f1fb98010374407fe0d71f
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-09-11 15:29:19 +0200

    backed up single geocoders

---
+++ source/plugins/_BAK_plg_irmapp_tocageocoder/assets/js/geo-helper.js
@@ -0,0 +1,507 @@
+/*
+Copyright 2013 devXive - research and development
+
+Version: 6.0.0 Timestamp: Mon Aug 12 15:04:12 PDT 2013
+
+This software is licensed under the devXive Proprietary Use License (the "devXive License") or the GNU
+General Public License version 2 (the "GPL License"). You may choose either license to govern your
+use of this software only upon the condition that you accept all of the terms of either the devXive
+License or the GPL License.
+
+You may obtain a copy of the devXive License and the GPL License at:
+
+    http://www.devxive.com/license
+    http://www.gnu.org/licenses/gpl-2.0.html
+
+Unless required by applicable law or agreed to in writing, software distributed under the
+devXive License or the GPL Licesnse is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the devXive License and the GPL License for
+the specific language governing permissions and limitations under the devXive License and the GPL License.
+*/
+	/*******************************************
+			SOLVER Functions
+	*******************************************/
+	/* 
+	 * Function to check wether or not if all sets are ok. This is the main to check for everything!
+	 */
+	function geoSolver( order, copyCheck ) {
+		// set validRouteAddresses to true, they will be overwritten by the addr_latlng check below (prevents the execution of the verifyRoute function until both are valid)
+		var validRouteAddresses = true,
+		    checkBack = {},
+		    dirHelper = '';
+
+		// Check the directions
+		if( tocaQuery.directions[0] === 'b' ) {
+			dirHelper = 'SINGLE';
+		} else {
+			dirHelper = 'ROUTE';
+		}
+
+
+		// Loop through directions
+		for ( var i in tocaQuery.directions ) {
+			// Get the address values
+			var addr = getAddress( order, tocaQuery.directions[i] );
+
+			if( !addr.address_lat || !addr.address_lng ) {
+				validRouteAddresses = false;
+
+				checkBack.address = addr;
+				checkBack.order = order;
+				checkBack.direction = tocaQuery.directions[i];
+
+				setTimeout(function() {
+					verifyAddress( checkBack, copyCheck );
+				}, 500);
+				break;
+			}
+		}
+
+		if( validRouteAddresses !== false ) {
+			var usher = getUsher( order );
+
+			if( dirHelper === 'ROUTE' ) {
+				// ##### ROUTE #####
+				var route = getRoute( order );
+
+				// Set the geoIcon based on the geo coordinates we have
+				setGeoIcon( order, route );
+
+				if( !route.estimated_time && !route.estimated_distance ) {
+					checkBack.route = route;
+					checkBack.order = order;
+
+					// Adjusting milliseconds to prevent closing of second window if both addresses have to verify
+					setTimeout(function() {
+						verifyRoute( checkBack, copyCheck );
+					}, 500);
+				} else {
+					// Turn off the lights
+					$(usher.order + ' .loader').hide();
+
+					if( !copyCheck ) {
+						// Fields are checked, set readonly
+						$(usher.order + ' .input-control').attr('readonly', true);
+						$(usher.order + ' select').select2('readonly', true);
+
+						// Go back to autoCheck and remove the position/orderId from the checkArray and take next, if one exists
+						autoCheck( order, true );
+					} else {
+						trans_copy( order, true );
+					}
+				}
+			} else {
+				// ##### SINGLE ADDRESS #####
+				console.log('Weiter zum Speichern');
+
+				// Goto saveCheck function for trashing the orderKey/position/id...
+			//	saveCheck( order );
+			}
+		}
+	}
+
+
+	/*
+	 * Funtion to verify the address right from the gmap3 source code to catch and validate address components and also the geo latlng
+	 */
+	function verifyAddress( checkBack, copyCheck ) {
+		var opDir = '';
+		if( checkBack.direction === 'f' ) {
+			opDir = 'Origin';
+		} else if( checkBack.direction === 't' ) {
+			opDir = 'Destination';
+		} else {
+			opDir = 'Home';
+		}
+		alertify.log('<i class="icon-globe icon-large"></i> Checking ' + opDir + ' address...');
+
+		$('#map-canvas').gmap3({
+			getlatlng: {
+				address: checkBack.address.string_name,
+				callback: function(results, status) {
+					if( status == google.maps.GeocoderStatus.OK ) {
+						var addressHelper = {},
+						    addressArray  = {};
+
+						var geometry = results[0].geometry,
+						    position = geometry.location,
+						    address  = results[0].address_components;
+
+						 var   positions = results[0].geometry.location
+
+						// Format latlng and reject to object
+						var posCounter = 0, posHelper = {};
+						for ( var i in positions ) {
+							if( i.match(/^.b$/) ) {
+								posHelper[posCounter] = positions[i];
+								posCounter++;
+							}
+						}
+
+						// Set the lat (from Format latlng and reject to object)
+						addressArray.address_lat = posHelper[0];
+						// Set the lng (from Format latlng and reject to object)
+						addressArray.address_lng = posHelper[1];
+
+						// Preformat the new address, we need to throw in input fields and for address comparison
+						jQuery.each(address, function(key, val) {
+							$.each(val.types, function(key2, val2) {
+								addressHelper[val2] = val.long_name;
+							});
+						});
+						// Set the street ( route ) if given
+						addressArray.address_street = (addressHelper.route) ? addressHelper.route : '';
+						// Set the street ( street_number ) if given
+						addressArray.address_houseno = (addressHelper.street_number) ? addressHelper.street_number : '';
+						// Set the zip ( postal_code ) if given
+						addressArray.address_zip = (addressHelper.postal_code) ? addressHelper.postal_code : '';
+						// Set the city ( locality, political ) if given
+						addressArray.address_city = (addressHelper.locality) ? addressHelper.locality : '';
+						// Set the region ( administrative_area_level_1, political ) if given
+						addressArray.address_region = (addressHelper.administrative_area_level_1) ? addressHelper.administrative_area_level_1 : '';
+						// Set the country ( country, political ) if given
+						addressArray.address_country = (addressHelper.country) ? addressHelper.country : '';
+
+						// Compare addresses
+						var comparisonCheck = compareAddress( checkBack.address, addressArray, checkBack.direction );
+						if( comparisonCheck != true ) {
+							alertify.set({
+								buttonFocus : 'none'
+							});
+							alertify.confirm(comparisonCheck, function(e) {
+								if( e ) {
+									// User click ok
+									setNewAddress( checkBack.order, checkBack.direction, addressArray );
+
+									// Move back to the solver
+									geoSolver( checkBack.order, copyCheck );
+								} else {
+									// User click cancel inject 0 as geopos for latLng to prevent further checks
+									checkBack.address.address_lat = 0;
+									checkBack.address.address_lng = 0;
+									setNewAddress( checkBack.order, checkBack.direction, checkBack.address );
+
+									// Move back to the solver
+									geoSolver( checkBack.order, copyCheck );
+								}
+							});
+						} else {
+							setNewCoordinates( checkBack.order, checkBack.direction, addressArray );
+							setTimeout(function() {
+								geoSolver( checkBack.order, copyCheck );
+							}, 500);
+						}
+					} else {
+						alertify.error( '<i class=\"icon-warning-sign icon-large\"></i> Transport: ' + checkBack.order + ' An error occured on geocoding. May the service is temporary unavailable. Automatic retry in 30 seconds!' );
+						setTimeout(function() {
+							geoSolver( checkBack.order, copyCheck );
+						}, 30000);
+					}
+				}
+			}
+		});
+	}
+
+
+	/*
+	 * Funtion to verify the route right from the gmap3 source code to catch the estimated duration and distance
+	 */
+	function verifyRoute( checkBack, copyCheck ) {
+		alertify.log('<i class="icon-globe icon-large"></i> Build Route...');
+		$('#map-canvas').gmap3({
+			getdistance: {
+				options: {
+					origins: [checkBack.route.origin.string_name],
+					destinations: [checkBack.route.destination.string_name],
+					travelMode: google.maps.TravelMode.DRIVING
+				},
+				callback: function( results, status ){
+					var usher = getUsher( checkBack.order, null );
+
+					if (results){
+						for (var i = 0; i < results.rows.length; i++){
+							var elements = results.rows[i].elements;
+							for(var j=0; j<elements.length; j++) {
+								switch(elements[j].status) {
+									case "OK":
+										var duration = elements[j].duration;
+										var distance = elements[j].distance;
+
+										results.estimates = { 'duration': duration, 'distance': distance };
+
+										// Route is verified, set icon to green and set text to info icons
+										$(usher.order + ' span.manurouter').hide();
+										$(usher.order + ' span.duration span').html(results.estimates.duration.text);
+										$(usher.order + ' span.duration').fadeIn();
+										$(usher.order + ' span.distance span').html(results.estimates.distance.text);
+										$(usher.order + ' span.distance').fadeIn();
+
+										// setNewRoute and move back to the solver
+										setNewRoute( checkBack.order, results );
+										geoSolver( checkBack.order, copyCheck );
+										break;
+									case "NOT_FOUND":
+										alertify.error( '<i class=\"icon-warning-sign icon-large\"></i> Transport: ' + checkBack.order + ' The origin and/or destination of this pairing could not be geocoded.' );
+
+										results.estimates.duration.value = 0;
+										results.estimates.duration.text  = 'N/A';
+										results.estimates.distance.value = 0;
+										results.estimates.distance.text  = 'N/A';
+
+										$(usher.order + ' span.manurouter').removeClass('btn-danger').addClass('btn-warning');
+
+										// setNewRoute and move back to the solver
+										setNewRoute( checkBack.order, results );
+										geoSolver( checkBack.order, copyCheck );
+										break;
+									case "ZERO_RESULTS":
+										alertify.error( '<i class=\"icon-warning-sign icon-large\"></i> Transport: ' + checkBack.order + ' No route could be found between the origin and destination!' );
+
+										results.estimates.duration.value = 0;
+										results.estimates.duration.text  = 'N/A';
+										results.estimates.distance.value = 0;
+										results.estimates.distance.text  = 'N/A';
+
+										// setNewRoute and move back to the solver
+										setNewRoute( checkBack.order, results );
+										geoSolver( checkBack.order, copyCheck );
+										break;
+								}
+							}
+						}
+					} else {
+						alertify.error( '<i class=\"icon-warning-sign icon-large\"></i> Transport: ' + checkBack.order + ' An error occured on geocoding. May the service is temporary unavailable. Automatic retry in 30 seconds!' );
+						setTimeout(function() {
+							geoSolver( checkBack.order, copyCheck );
+						}, 30000);
+					}
+				}
+			}
+		});
+	}
+
+
+	function setGeoIcon( order, route ) {
+		var usher = getUsher ( order, null );
+
+		if( (route.origin.address_lat && route.origin.address_lng) && (route.destination.address_lat && route.destination.address_lng) ) {
+			jQuery(usher.order + ' span.geo-icon').removeClass('btn-danger').removeClass('btn-warning').addClass('btn-success');
+		} else if( (route.origin.address_lat && route.origin.address_lng) || (route.destination.address_lat && route.destination.address_lng) ) {
+			jQuery(usher.order + ' span.geo-icon').removeClass('btn-danger').removeClass('btn-success').addClass('btn-warning');
+		} else {
+			jQuery(usher.order + ' span.geo-icon').removeClass('btn-warning').removeClass('btn-success').addClass('btn-danger');
+		}
+	}
+
+
+
+
+
+	/*******************************************
+			Map Functions
+	*******************************************/
+	// Function to initialise the map with optional marker and a rendered address
+	function initialize(initLat, initLng, initZoom, initMarker) {
+		jQuery('#map-canvas').gmap3({
+			marker:{
+				latLng: (initMarker) ? [initLat, initLng] : false
+			},
+			map:{
+				options: {
+					zoom                   : initZoom,
+					center                 : [initLat, initLng],
+					mapTypeId              : google.maps.MapTypeId.ROADMAP,
+					draggable              : true,
+					scrollwheel            : false,
+					disableDefaultUI       : true,
+					disableDoubleClickZoom : true
+				}
+			}
+		});
+
+		// Clear the marker if initMarker == false or not set
+		if (!initMarker) {
+			jQuery('#map-canvas').gmap3({
+				clear:{
+					name:['marker'],
+					last: true
+				}
+			});
+		}
+	}
+
+
+	// Function to render a address and get back an object with formatted values. Also renders the new point on the map
+	function renderAddress_OLD(mapSelector, addr, direction, order) {
+		$('#' + mapSelector).gmap3({
+			getlatlng: {
+				address: addr,
+				callback: function(results, status) {
+					if( status == google.maps.GeocoderStatus.OK ) {
+						// Adding direction and order to results
+						var transport = {direction: direction, order: order};
+						results[0].transport = transport;
+
+						var geometry = results[0].geometry,
+						position     = geometry.location,
+						address      = results[0].address_components;
+
+						// Format latlng and reject to object
+						var posCounter = 0, posHelper = {};
+						jQuery.each(results[0].geometry.location, function(key, val) {
+							if( key.match(/^.b$/) ) {
+								posHelper[posCounter] = val;
+								posCounter++;
+							}
+						});
+						results[0].geometry.coords = {lat: posHelper[0], lng: posHelper[1]};
+
+						// Preformat the new address, we need to throw in input fields
+						var addressHelper = {};
+						jQuery.each(address, function(key, val) {
+							$.each(val.types, function(key2, val2) {
+								addressHelper[val2] = val.long_name;
+							});
+						});
+
+						var addressArray = {};
+						// Set the street ( route ) if given
+						addressArray.address_street = (addressHelper.route) ? addressHelper.route : '';
+						// Set the street ( street_number ) if given
+						addressArray.address_houseno = (addressHelper.street_number) ? addressHelper.street_number : '';
+						// Set the zip ( postal_code ) if given
+						addressArray.address_zip = (addressHelper.postal_code) ? addressHelper.postal_code : '';
+						// Set the city ( locality, political ) if given
+						addressArray.address_city = (addressHelper.locality) ? addressHelper.locality : '';
+						// Set the region ( administrative_area_level_1, political ) if given
+						addressArray.address_region = (addressHelper.administrative_area_level_1) ? addressHelper.administrative_area_level_1 : '';
+						// Set the country ( country, political ) if given
+						addressArray.address_country = (addressHelper.country) ? addressHelper.country : '';
+						results[0].input_address = addressArray;
+
+						// Clear the last and set the new marker
+						$(this).gmap3({
+							clear:{
+								name:['marker'],
+								last: true
+							},
+							marker:{
+								values:[
+									{latLng: position}
+								],
+								options:{
+									animation: google.maps.Animation.BOUNCE
+								}
+							},
+							map:{
+								options:{
+									center: position,
+									zoom: 17
+								}
+							}
+						});
+
+						// Throw the callback
+						callbackCodeAddress(results);
+					} else {
+						console.log('Geocode was not successful for the following reason: ' + status);
+						// Throw the callback
+						callbackCodeAddress(status);
+					}
+				}
+			}
+		});
+	}
+
+
+
+
+
+
+
+
+	/*******************************************
+			Render Map Functions
+	*******************************************/
+
+
+	/*
+	 * Funtion to render the route and direction on the google maps tab
+	 */
+	function renderRouteDirection_OLD(selector, order, dirCanvas) {
+		// Get the first transport directions
+		var addrF = getAddress(order, 'f');
+		var addrT = getAddress(order, 't');
+
+		// Check for lat lng values, set the icon color and init the map
+		if( addrF.address_lat && addrF.address_lng && addrT.address_lat && addrT.address_lng ) {
+			$('#' + selector).gmap3({
+				getroute: {
+					options: {
+						origin: addrF.string_name,
+						destination: addrT.string_name,
+						travelMode: google.maps.DirectionsTravelMode.DRIVING
+					},
+					callback: function( results ){
+						if (!results) return;
+						$(this).gmap3({
+							clear: 'directionsrenderer',
+							map:{
+								options:{
+									zoom: 13,
+									center: [50, 9]
+								}
+							},
+							directionsrenderer: {
+								container: $('#' + dirCanvas),
+								options: {
+									directions: results
+								}
+							}
+						});
+						console.log(results);
+					}
+				}
+			});
+		}
+	}
+
+
+
+
+
+	/*
+	 * Funtion to render the route on the main map-canvas div
+	 */
+	function renderRoute( order ) {
+		var route = getRoute( order );
+		$('#map-canvas').height('200px').width('100%').gmap3({
+			getroute: {
+				options: {
+					origin: route.origin.string_name,
+					destination: route.destination.string_name,
+					travelMode: google.maps.DirectionsTravelMode.DRIVING
+				},
+				callback: function( results, status ){
+					if( status == results.status ) {
+						$(this).gmap3({
+							clear: 'directionsrenderer',
+							map:{
+								options:{
+									zoom: 13,
+									center: [50, 9],
+									scrollwheel: false,
+									streetViewControl: false
+								}
+							},
+							directionsrenderer: {
+								options: {
+									directions: results
+								}
+							}
+						});
+					}
+				}
+			}
+		});
+	}
---
+++ source/plugins/_BAK_plg_irmapp_tocageocoder/tocageocoder.php
@@ -0,0 +1,271 @@
+<?php
+/**
+ * @package     IRM.Plugin
+ * @subpackage  IRMApp.tocageocoder
+ *
+ * @copyright   Copyright (C) 1997 - 2013 devXive - research and development. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ *
+ * @since       6.0
+ */
+
+defined('JPATH_BASE') or die;
+
+class PlgIrmAppTocageocoder extends JPlugin
+{
+	/**
+	 * Stores the app name
+	 * @var	appKey
+	 * @since	6.0
+	 */
+	var $appKey;
+
+	/**
+	 * Stores the app ACL
+	 * @var	acl
+	 * @since	6.0
+	 */
+	var $acl;
+
+
+	/**
+	 * INITIATE THE CONSTRUCTOR
+	 */
+	public function __construct(& $subject, $config)
+	{
+		parent::__construct($subject, $config);
+
+		// TODO: Check first if we have an internet connection, else return false or throw message htat there is no connection and that this geocoder doesn't work
+		// NFWConnectionHelper::checkOnline(); or something else
+
+		$this->appKey = 'tocageocoder';
+		$this->loadLanguage();
+	}
+
+
+	/**
+	 * @param   object	&$item		The item referenced object which includes the system id of this transport
+	 *
+	 * @return  array			appKey = The tab identification, tabContent = Content of the Container
+	 *
+	 * @since   3.0
+	 */
+	public function onBeforeContent( &$item = null, &$params = null )
+	{
+		// Init google maps and jquery gmap3 plugin and helper js file
+		NFWGeoGmap3::initMap();
+		JHtml::_('script', 'plugins/irmapp/tocageocoder/assets/js/geo-helper.js', false, false, false, false);
+
+		NFWHtmlJavascript::detectChanges();
+
+		// Set the script
+		$script = "
+			/*
+			 * Function to get the item cid (db id of the core item we are in. Should always ends --regex selector $-- with _cid)
+			 */
+			function getItemId() {
+				var itemId = jQuery('form[data-order=\"1\"] input[id*=\"_cid-\"]').val();
+				if( itemId && itemId > 0 ) {
+					return itemId;
+				} else {
+					return false;
+				}
+			}
+
+
+			/*
+			 * Global ready function
+			 */
+			jQuery(document).ready(function() {
+				// If user click edit button, we have to show the input field and graphical stuff
+				$('#loading-btn-edit').click(function() {
+					$(document).ajaxSuccess(function() {
+//						$('.address_auto_geocoder').fadeIn('slow');
+					});
+				});
+
+				// If user click update/save button, we have to hide the input field and graphical stuff
+				$('#loading-btn-save').click(function() {
+					$(document).ajaxSuccess(function() {
+//						$('.address_auto_geocoder, .easypie-progress').hide();
+					});
+				});
+
+
+
+				// ##################### START OF TRIGGERED EVENTS TO THE GEOCODING PROCESS #########################
+				// Triggered on every change in the auto geocoder input field but only if its focused (this.value determines the actual field)
+//				$('.address_auto_geocoder input').focus(function() {
+//					// Get the direction and order position
+//					var ownOrder = $(this).parents('form').data('order');
+//					var ownDirection = $(this).parents('.address-block').data('direction');
+//					var nameObserver = 'form[data-order=\"' + ownOrder + '\"] .address-block[data-direction=\"' + ownDirection + '\"]';
+//
+//					$(nameObserver + ' .geocode-input-helptext').slideToggle();
+//
+//					$(this).on('inputchange', function() {
+//						window.clearTimeout(timeoutId);
+//						var addressValue = $(nameObserver + ' .address_auto_geocoder input').val();
+//
+//						// Trigger only if at least 5 digits typed in
+//						if( addressValue.length > 5 ) {
+//							timeoutId = window.timeoutId = setTimeout(function() {
+//								codeAddress(addressValue, ownDirection, ownOrder);
+//							}, 1500);
+//
+//							// If anything in this field has changed play with the animation
+//							$('.easypie-progress.ep-chart').data('easyPieChart').update(100);
+//							$('.easypie-progress.ep-chart').data('easyPieChart').update(0);
+//						}
+//					});
+//				}).focusout(function() {
+//					// Get the direction and order position
+//					var ownOrder = $(this).parents('form').data('order');
+//					var ownDirection = $(this).parents('.address-block').data('direction');
+//					var nameObserver = 'form[data-order=\"' + ownOrder + '\"] .address-block[data-direction=\"' + ownDirection + '\"]';
+//
+//					$(nameObserver + ' .geocode-input-helptext').slideToggle();
+//				});
+
+
+				// Triggered on every change in the inner-address-block (this.value determines the actual field) NOTE: METHODS ONLY FOR SINGLE EDITS!
+				if( getItemId() ) {
+					// Render the Map Route if estimated values are set else, show BIG verify button
+					var singleRoute = getRoute(1);
+					$('#map-body').addClass('widget-main').addClass('padding-5');
+
+					if( singleRoute.estimated_time && singleRoute.estimated_distance ) {
+						$('#map-canvas').height('250px').width('100%');
+
+						renderRoute( singleRoute );
+					} else {
+						var htmlButton = '<a onClick=\"verifyRoute( getRoute(1) )\" id=\"verifyroutebutton\" class=\"btn btn-large btn-block\" style=\"font-size: 200%;\">';
+							htmlButton += '<i class=\"icon-compass icon-spin\"></i> Verify Route';
+						htmlButton += '</a>';
+						$('#map-canvas').html(htmlButton);
+					}
+
+					// Directions Observer
+					$('.inner-address-block input').on('inputchange', function() {
+						// Get the direction and order position
+						var ownOrder = $(this).parents('form').data('order');
+						var ownDirection = $(this).parents('.address-block').data('direction');
+
+						var masterUsher = 'form[data-order=\"' + ownOrder + '\"] .address-block[data-direction=\"' + ownDirection + '\"]';
+						var orderUsher = 'form[data-order=\"' + ownOrder + '\"]';
+
+						// User changed something, kick off hash, latlng and estimates
+						$(masterUsher + ' input[id*=\"address_lat\"]').val('');
+						$(masterUsher + ' input[id*=\"address_lng\"]').val('');
+
+						$(orderUsher + ' input[id*=\"estimated_time\"]').val('');
+						$(orderUsher + ' input[id*=\"estimated_distance\"]').val('');
+					});
+				} else {
+				}
+			});
+		";
+		JFactory::getDocument()->addScriptDeclaration($script);
+	}
+
+
+
+	/**
+	 * @param   object	&$item		The item referenced object which includes the system id of this transport
+	 *
+	 * @return  array			appKey = The tab identification, tabContent = Content of the Container
+	 *
+	 * @since   3.0
+	 */
+	public function htmlBuildWidgetTop( &$item = null, &$params = null )
+	{
+		// Get Permissions based on category
+		if ( !$item->catid ) {
+			// We have no category id and use the components acl
+			$this->acl = NFWAccessHelper::getActions('com_xiveirm');
+		} else {
+			// We have a category id and use the category acl
+			$this->acl = NFWAccessHelper::getActions('com_xiveirm', 'category', $item->catid);
+		}
+
+		ob_start();
+		?>
+		<!---------- Begin output buffering: <?php echo $this->appKey; ?> ---------->
+
+		<!---------- End output buffering: <?php echo $this->appKey; ?> ---------->
+		<?php
+
+		$html = ob_get_clean();
+
+		$inMasterContainer = array(
+			'appKey' => $this->appKey,
+			'html' => $html
+		);
+
+		return $inMasterContainer;
+	}
+
+
+	/**
+	 * Method to inject a tab with form fields to extend the core form. The position of this tab is the same as the buttons, based on the plugin position.
+	 *
+	 * @param     object    &$item      The item referenced object which includes the system id of this contact
+	 * @param     object    &$params    The params referenced object
+	 *
+	 * @return    array                 Array with the $appKey, the $html data for rendering and the $tabButton to show at the top.
+	 *                                  tabButton - Use either a name and/or a translatable string with or without an icon
+	 *
+	 * @since     6.0
+	 */
+	public function htmlBuildTab( &$item = null, &$params = null )
+	{
+		$googleMapIcon = '/plugins/irmapp/tocageocoder/assets/img/googlemaps-icon.png';
+
+		ob_start();
+		?>
+		<script>
+			/*
+			 * Render map on the large tab view
+			 */
+			jQuery('.tocageocoder_tabbutton').click(function() {
+				renderRouteDirection('tabmap-canvas', 1, 'direction-canvas');
+			});
+		</script>
+
+		<!---------- Begin output buffering: <?php echo $this->appKey; ?> ---------->
+
+		<div class="row-fluid">
+			<div class="span7">
+				<div id="tabmap-canvas" style="height: 450px; width: 100%;"></div>
+			</div>
+			<div class="span5">
+				<div id="direction-canvas" class="googlemap" style="width: 100%;"></div>
+			</div>
+
+			<div class="hr"></div>
+
+			<center>
+				<span class="help-button xpopover" data-trigger="hover" data-placement="top" data-content="Informations given here are used in other applications, such as the despatching app => order form. Use this as help to minimize inputs during remaining phone orders." data-original-title="Info about cross referencing!"><i class="icon-random"></i></span>
+			</center>
+		</div>
+
+		<!---------- End output buffering: <?php echo $this->appKey; ?> ---------->
+
+		<?php
+		$html = ob_get_clean();
+
+		// Create the tabbed button
+		$tabButton = '<img src="' . $googleMapIcon . '" style="height: 15px; margin-top: -2px;"> ' . JText::_('Google Maps');
+
+		$eventArray = array(
+			'appKey'    => $this->appKey,
+			'tabButton' => $tabButton,
+			'tabBody'   => $html
+		);
+
+		if ( $item->id ) {
+			return $eventArray;
+		}
+	}
+}
+?>
